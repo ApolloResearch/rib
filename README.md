f7a5e685fcfd6f0ce6e86a6ccbcfb23406d992d9
danbraunai <brauny_13@hotmail.com>
2023-06-16 17:21:14 +0100

    Add setup.py and vscode settings

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rib
-Library for methods related to Rotation into the Interaction Basis
+
+Library containing methods related to Rotation into the Interaction Basis.
+
+## Installation
+
+From the root of the repository, run
+
+```bash
+pip install -e .
+```
+
+## Development
+
+Suggested extensions and settings for VSCode are provided in `.vscode/`.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+if Path("requirements.txt").exists():
+    requirements = Path("requirements.txt").read_text("utf-8").splitlines()
+else:
+    requirements = []
+
+setup(
+    name="rib",
+    version="0.0.1",
+    description="Library for the Rotation into the Interaction Basis (RIB) method.",
+    long_description=Path("README.md").read_text("utf-8"),
+    author="Dan Braun",
+    author_email="dan@apolloresearch.ai",
+    url="https://github.com/ApolloResearch/rib",
+    packages=find_packages(where=["rib"]),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)

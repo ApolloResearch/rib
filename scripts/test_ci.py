6d18880cdd19985bc8efbea82bc9515e43c90e5d
danbraunai <brauny_13@hotmail.com>
2023-06-18 15:03:37 +0100

    test commit

---
+++ scripts/test_ci.py
@@ -0,0 +1,16 @@
+import torch
+
+
+class MLP(torch.nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.linear = torch.nn.Linear(10, 10)
+
+    def forward(self, x):
+        return self.linear(x)
+
+
+if __name__ == "__main__":
+    model = MLP()
+    print(model)
+    print(model(torch.zeros(10)))

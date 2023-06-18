2000bd68bcf3be438406801b9cd5b26b1f039620
danbraunai <brauny_13@hotmail.com>
2023-06-18 12:44:42 +0100

    fixed typing

--- scripts/test_ci.py
+++ scripts/test_ci.py
@@ -3,12 +3,12 @@ import torch
 
 # Define an MLP
 class MLP(torch.nn.Module):
-    def __init__(self, input_size: int, hidden_size: int, output_size):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int) -> None:
         super().__init__()
         self.fc1 = torch.nn.Linear(input_size, hidden_size)
         self.fc2 = torch.nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = torch.nn.functional.relu(self.fc1(x))
         x = self.fc2(x)
         return x

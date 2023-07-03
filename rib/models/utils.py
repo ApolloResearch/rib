c09a8d76699376984d01efbbe9a0e08f33c38ece
Dan Braun <dan@apolloresearch.ai>
2023-07-03 14:16:38 +0000

    Fix naming of MLP layers

--- rib/models/utils.py
+++ rib/models/utils.py
@@ -2,7 +2,6 @@ from typing import Union
 
 import torch
 
-ACTIVATION = Union[torch.nn.ReLU, torch.nn.GELU, torch.nn.Tanh, torch.nn.Sigmoid]
 ACTIVATION_MAP = {
     "relu": torch.nn.ReLU,
     "gelu": torch.nn.GELU,

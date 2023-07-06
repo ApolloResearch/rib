71a708af53d889d3add0c64d71ca9cde8a2deaf2
Dan Braun <dan@apolloresearch.ai>
2023-07-06 11:09:06 +0000

    Add zero_threshold eigenvalue info and refactor

--- rib/linalg.py
+++ rib/linalg.py
@@ -1,8 +1,11 @@
-from typing import Tuple
+from dataclasses import dataclass
+from typing import Optional, Tuple
 
 import torch
 from jaxtyping import Float
 from torch import Tensor
+
+from rib.hooks import HookedModel
 
 
 def eigendecompose(
@@ -31,3 +34,48 @@ def eigendecompose(
         eigenvalues = eigenvalues[idx]
         eigenvectors = eigenvectors[:, idx]
     return eigenvalues, eigenvectors
+
+
+@dataclass
+class EigenInfo:
+    """Eigenvalues, eigenvectors, and number of zero eigenvalues of a matrix.
+
+    Attributes:
+        vals: Eigenvalues of a matrix.
+        vecs: Eigenvectors of a matrix.
+        zero_vals: Number of eigenvalues that are zero.
+    """
+
+    vals: Float[Tensor, "d_hidden"]
+    vecs: Float[Tensor, "d_hidden d_hidden"]
+    zero_vals: Optional[int] = None
+
+
+def calc_eigen_info(
+    hooked_mlp: HookedModel,
+    hook_points: list[str],
+    matrix_key: str = "gram",
+    zero_threshold: Optional[float] = 1e-13,
+) -> dict[str, EigenInfo]:
+    """Calculate eigenvalues and eigenvectors of the gram matrices for each hook point.
+
+    Args:
+        hooked_mlp: The hooked model.
+        hook_points: The hook points for which to calculate the eigenvalues and eigenvectors.
+        matrix_key: The key used to store the gram matrix in the hooked data.
+        zero_threshold: The threshold below which eigenvalues are considered to be zero. If None,
+            no thresholding is performed.
+
+    Returns:
+        A dictionary of EigenInfo objects, keyed by hook point.
+    """
+    eigens = {}
+    for hook_point in hook_points:
+        gram_matrix = hooked_mlp.hooked_data[hook_point][matrix_key]
+        eigenvalues, eigenvectors = eigendecompose(gram_matrix, descending=True)
+        zero_vals = (
+            torch.sum(eigenvalues < zero_threshold).item() if zero_threshold is not None else None
+        )
+        eigens[hook_point] = EigenInfo(vals=eigenvalues, vecs=eigenvectors, zero_vals=zero_vals)
+
+    return eigens
--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -1,7 +1,9 @@
+from unittest import mock
+
 import pytest
 import torch
 
-from rib.linalg import eigendecompose
+from rib.linalg import EigenInfo, calc_eigen_info, eigendecompose
 
 
 @pytest.mark.parametrize("descending", [True, False])
@@ -39,3 +41,44 @@ def test_eigendecompose(descending: bool):
         assert torch.all(eigenvalues[:-1] >= eigenvalues[1:])
     else:
         assert torch.all(eigenvalues[:-1] <= eigenvalues[1:])
+
+
+@pytest.mark.parametrize("zero_threshold", [None, 1e-1])
+def test_calc_eigen_matrices(zero_threshold):
+    """Test the calc_eigen_matrices function.
+
+    Checks if the EigenInfo objects for each hook point are correctly computed.
+
+    Args:
+        zero_threshold: The threshold below which eigenvalues are considered to be zero.
+    """
+    # Define mock data
+    d_hidden = 20
+    gram_matrix = torch.randn(d_hidden, d_hidden)
+    gram_matrix = gram_matrix @ gram_matrix.T  # Ensure gram_matrix is symmetric
+
+    hooked_data = {"hook_point_1": {"gram": gram_matrix}, "hook_point_2": {"gram": gram_matrix}}
+
+    # Create mock hooked model
+    hooked_mlp = mock.Mock()
+    hooked_mlp.hooked_data = hooked_data
+
+    hook_points = list(hooked_data.keys())
+    result = calc_eigen_info(hooked_mlp, hook_points, zero_threshold=zero_threshold)
+
+    # Check if keys in the result match hook_points
+    assert set(result.keys()) == set(hook_points)
+
+    # Check the results
+    for hook_point in hook_points:
+        eigen_info = result[hook_point]
+
+        assert isinstance(eigen_info, EigenInfo)
+        assert eigen_info.vals.dtype == torch.float64
+        assert eigen_info.vecs.dtype == torch.float64
+
+        # If zero_threshold is provided, zero_vals should be computed
+        if zero_threshold is not None:
+            assert eigen_info.zero_vals == torch.sum(eigen_info.vals < zero_threshold).item()
+        else:
+            assert eigen_info.zero_vals is None

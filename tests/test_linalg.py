709772fd4bdff0d8d3bd26dfaaea15de6bda0fcf
Dan Braun <dan@apolloresearch.ai>
2023-07-09 10:53:53 +0100

    Refactor mnist ablation (#23)
    * Refactor mnist ablation
    
    * Remove unused eigenvalue info and fix plotting

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -1,9 +1,7 @@
-from unittest import mock
-
 import pytest
 import torch
 
-from rib.linalg import EigenInfo, calc_eigen_info, calc_rotation_matrix, eigendecompose
+from rib.linalg import calc_rotation_matrix, eigendecompose
 
 
 @pytest.mark.parametrize("descending", [True, False])
@@ -90,49 +88,3 @@ def test_calc_rotation_matrix(n_zero_vals, n_ablated_vecs):
         acts_eigenspace[:, -n_ignore:] = 0
     rotated_vecs_2 = acts_eigenspace @ vecs.T
     assert torch.allclose(rotated_vecs, rotated_vecs_2, atol=1e-6)
-
-
-@pytest.mark.parametrize("zero_threshold,n_ablated_vecs", [(None, 10), (1e-13, 0), (1e-6, 5)])
-def test_calc_eigen_info(zero_threshold, n_ablated_vecs):
-    """Test the calc_eigen_info function.
-
-    Checks if the function correctly calculates and returns EigenInfo objects for all given hook
-    points.
-    """
-    torch.manual_seed(0)
-    # Mock the hooked model
-    hooked_mlp = mock.Mock()
-    hooked_mlp.hooked_data = {
-        "hook1": {"gram": torch.randn(10, 10)},
-        "hook2": {"gram": torch.randn(10, 10)},
-    }
-
-    # Define hook points for calc_eigen_info
-    hook_points = ["hook1", "hook2"]
-
-    if zero_threshold is not None and n_ablated_vecs > 0:
-        with pytest.raises(AssertionError):
-            calc_eigen_info(
-                hooked_mlp,
-                hook_points,
-                zero_threshold=zero_threshold,
-                n_ablated_vecs=n_ablated_vecs,
-            )
-        return
-    # Calculate eigen information
-    eigen_infos = calc_eigen_info(
-        hooked_mlp, hook_points, zero_threshold=zero_threshold, n_ablated_vecs=n_ablated_vecs
-    )
-
-    # Check the results
-    assert len(eigen_infos) == len(hook_points)
-    for hook_point in hook_points:
-        eigen_info = eigen_infos[hook_point]
-
-        # Assert instance of EigenInfo
-        assert isinstance(eigen_info, EigenInfo)
-
-        # Check dimensions of EigenInfo values
-        assert eigen_info.vals.shape == (10,)
-        assert eigen_info.vecs.shape == (10, 10)
-        assert eigen_info.rotation_matrix.shape == (10, 10)

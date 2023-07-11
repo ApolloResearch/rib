dc0e289a6e4ee1d15e7d653163d38109be5ae6a0
Dan Braun <dan@apolloresearch.ai>
2023-07-11 12:46:11 +0100

    Feature/Allow get_attr in module lists (#27)
    * Make MLP use a module list
    
    * Handle ModuleLists in get_model_attr function

--- rib/models/utils.py
+++ rib/models/utils.py
@@ -29,25 +29,46 @@ def save_model(config_dict: dict[str, Any], save_dir: Path, model: nn.Module, ep
 def get_model_attr(model: torch.nn.Module, attr_path: str) -> torch.nn.Module:
     """Retrieve a nested attribute of a PyTorch module by a string of attribute names.
 
-    Navigates through the model's structure following the provided attribute path. Each attribute
-    name in the path is separated by a period ('.').
+    Each attribute name in the path is separated by a period ('.').
+
+    Since models often have lists of modules, the attribute path can also include an index.
 
     Args:
         model (torch.nn.Module): The PyTorch model.
         attr_path (str): A string representing the path to the attribute.
 
     Returns:
-        torch.nn.Module: The attribute (which may be a module or other object) at the specified path
-            in the model.
+        torch.nn.Module: The attribute of the model.
 
     Example:
-        >>> mlp = MLP(...)
-        >>> linear_layer = get_model_attr(mlp, "layers.linear_1")
-        >>> print(linear_layer)
-        Linear(in_features=100, out_features=10, bias=False)
+        >>> mlp = MLP([5], input_size=2, output_size=3)
+        >>> mlp
+        MLP(
+            (layers): ModuleList(
+                (0): Layer(
+                    (linear): Linear(in_features=2, out_features=5, bias=True)
+                    (activation): ReLU()
+                )
+                (1): Layer(
+                    (linear): Linear(in_features=5, out_features=3, bias=True)
+                )
+            )
+        )
+        - get_model_attr(model, "layers") -> ModuleList(...)
+        - get_model_attr(model, "layers.0") -> Layer(...)
+        - get_model_attr(model, "layers.0.activation") -> ReLU()
+        - get_model_attr(model, "layers.1.linear") -> LinearFoldedBias(...)
     """
     attr_names = attr_path.split(".")
     attr = model
+
     for name in attr_names:
-        attr = getattr(attr, name)
+        try:
+            if isinstance(attr, torch.nn.ModuleList) and name.isdigit():
+                attr = attr[int(name)]
+            else:
+                attr = getattr(attr, name)
+        except AttributeError:
+            logger.error(f"Attribute '{name}' not found in the path '{attr_path}'.")
+            raise
     return attr

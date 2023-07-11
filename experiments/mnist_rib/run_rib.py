b4aca98ba4e057260fab72141ef12999a97403ff
Dan Braun <dan@apolloresearch.ai>
2023-07-11 18:07:50 +0100

    WIP: start adding interaction matrix calculation

--- experiments/mnist_rib/run_rib.py
+++ experiments/mnist_rib/run_rib.py
@@ -1,25 +1,27 @@
-"""Run an mlp on MNIST while rotating to and from a (truncated) orthogonal basis.
+"""Calculate the interaction basis for each layer of an MLP trained on MNIST.
 
 The process is as follows:
     1. Load an MLP trained on MNIST and a test set of MNIST images.
     2. Calculate gram matrices at the given hook points.
+    3. Eigendecompose the gram matrices. For the final layer, the interaction basis is equal to the
+        eigenbasis of the gram matrix.
+    4. (for other layers) TODO: complete
     3. Calculate a rotation matrix at each hook point representing the operation of rotating to and
-       from the partial eigenbasis of the gram matrix. The partial eigenbasis is equal to the entire
-       eigenbasis with the zeroed out eigenvectors corresponding to the n smallest eigenvalues,
-       where we let n range from 0 to the total number of eigenvalues (i.e. the dimension of the
-       gram matrix).
+       from the partial interaction basis of the gram matrix. The partial interaction basis is equal
+       to the entire interaction basis with the zeroed out columns corresponding to the n smallest
+       interactions, where we let n range from 0 to the total number of nodes (i.e. the dimension of
+       the gram matrix).
     4. Run the test set through the MLP, applying the rotations at each hook point, and calculate
        the resulting accuracy.
     5. Repeat steps 3 and 4 for a range of values of n, plotting the resulting accuracies.
 
 Usage:
-    python run_ablations.py <path/to/yaml_config_file>
+    python run_rib.py <path/to/yaml_config_file>
 
-This script will take 4 minutes to run on cpu or gpu for 2-layer 100-hidden-unit MLPs with two hook
-points.
 """
 
 import json
+from dataclasses import dataclass
 from pathlib import Path
 
 import fire
@@ -58,6 +60,22 @@ class Config(BaseModel):
     hook_configs: list[HookConfig]
 
 
+@dataclass
+class EigenInfo:
+    """Information about the eigendecomposition of a gram matrix."""
+
+    hook_name: str
+    eigenvals: Float[Tensor, "d_hidden"]
+    eigenvecs: Float[Tensor, "d_hidden d_hidden"]
+
+@dataclass
+class InteractionInfo:
+    """Information about the interaction basis of a gram matrix."""
+
+    hook_name: str
+    interaction_basis: Float[Tensor, "d_hidden d_hidden"]
+
+
 def load_mlp(config_dict: dict, mlp_path: Path) -> MLP:
     mlp = MLP(
         hidden_sizes=config_dict["model"]["hidden_sizes"],
@@ -80,30 +98,41 @@ def load_mnist_dataloader(train: bool = False, batch_size: int = 64) -> DataLoad
     return test_loader
 
 
-def ablate_and_test(
+def calc_interaction_matrices(
     hooked_mlp: HookedModel,
-    hook_config: HookConfig,
+    hook_configs: list[HookConfig],
+    eigen_infos: list[EigenInfo],
     test_loader: DataLoader,
     eigenvecs: Float[Tensor, "d_hidden d_hidden"],
     device: str,
-) -> list[float]:
-    """Ablate eigenvectors and test the model accuracy.
+) -> list[InteractionInfo]:
+    """Calculate the interaction matrix for each layer of the MLP.
+
+    We assume that the hook_configs (and eigen_infos) are in the same order as the modules of the
+    network. Importantly, we also assume that no computation happens between these modules. I.e.
+    the input to the 2nd layer is the output of the 1st layer, and so on.
 
     Args:
         hooked_mlp: The hooked model.
-        hook_config: The config for the hook point.
+        hook_configs: The configs for the hook points.
+        eigen_infos: The eigen information for each layer.
         test_loader: The DataLoader for the test data.
-        eigenvecs: A matrix whose columns are the eigenvectors of the gram matrix.
         device: The device to run the model on.
 
     Returns:
-        A list of accuracies for each number of ablated vectors.
+        A list of objects containing the interaction matrix for each layer.
     """
-    assert hook_config.hook_type in ["forward", "pre_forward"]
-    rotate_hook_fn_name = "rotate_" + hook_config.hook_type + "_hook_fn"
 
-    accuracies: list[float] = []
 
+    interaction_info: list[InteractionInfo] = []
+    # Iterate through the configs in backwards order
+    for i in range(len(hook_configs) - 1, -1, -1):
+        if i == len(hook_configs) - 1:
+            # The interaction matrix for the final layer is equal to the eigenbasis.
+            interaction_matrix = eigen_infos[i].eigenvecs
+        else:
+            # Get the jacobian of the next layer w.r.t the outputs of the current layer.
+            jac = get_jacobian(hook_configs[i + 1])
     # Iterate through possible number of ablated vectors.
     for n_ablated_vecs in tqdm(
         range(hook_config.layer_size + 1),
@@ -127,10 +156,10 @@ def ablate_and_test(
     return accuracies
 
 
-def run_ablations(
+def run_interaction_algorithm(
     model_config_dict: dict, mlp_path: Path, hook_configs: list[HookConfig]
 ) -> dict[str, list[float]]:
-    """Rotate to and from orthogonal basis and compare accuracies with and without ablation.
+    """Calculate the interaction basis for each layer of an MLP trained on MNIST.
 
     Args:
         model_config_dict: The config dictionary used for training the mlp.
@@ -156,31 +185,32 @@ def run_ablations(
                 data_key="gram",
                 fn_name=gram_hook_fn_name,
                 module_name=hook_config.module_name,
-            )
-        )
+            )listsList[])
 
-    test_loader = load_mnist_dataloader(train=False, batch_size=512)
-
+    test_loader = load_mnist_dataloader(
+        train=False, batch_size=model_config_dict["train"]["batch_size"]
+    )
     run_dataset_through_model(hooked_mlp, test_loader, gram_hooks, device=device)
     len_dataset = len(test_loader.dataset)  # type: ignore
 
+    eigen_info: list[EigenInfo] = []
     results: dict[str, list[float]] = {}
     for hook_config in hook_configs:
         # Scale the gram matrix by the number of samples in the dataset.
         hooked_mlp.hooked_data[hook_config.hook_name]["gram"] = (
             hooked_mlp.hooked_data[hook_config.hook_name]["gram"] / len_dataset
         )
-        _, eigenvecs = eigendecompose(hooked_mlp.hooked_data[hook_config.hook_name]["gram"])
-        accuracies: list[float] = ablate_and_test(
-            hooked_mlp=hooked_mlp,
-            hook_config=hook_config,
-            test_loader=test_loader,
-            eigenvecs=eigenvecs,
-            device=device,
-        )
-        results[hook_config.hook_name] = accuracies
+        eigenvals, eigenvecs = eigendecompose(hooked_mlp.hooked_data[hook_config.hook_name]["gram"])
+        eigen_info.append(EigenInfo(hook_name=hook_config.hook_name, eigenvals=eigenvals, eigenvecs=eigenvecs))
 
-    return results
+    interaction_bases = calc_interaction_matrices(
+        hook_mlp=hooked_mlp,
+        hook_configs=hook_configs,
+        eigen_info=eigen_info,
+        test_loader=test_loader,
+        device=device,
+    )
+    return interaction_bases
 
 
 def main(config_path_str: str) -> None:
@@ -190,14 +220,14 @@ def main(config_path_str: str) -> None:
     with open(config.mlp_path.parent / "config.yaml", "r") as f:
         model_config_dict = yaml.safe_load(f)
 
-    out_file = Path(__file__).parent / "out" / f"{config.mlp_name}_ablation_results.json"
+    out_file = Path(__file__).parent / "out" / f"{config.mlp_name}_rib_results.json"
     if out_file.exists():
         logger.error(f"Output file {out_file} already exists. Exiting.")
         return
 
     out_file.parent.mkdir(parents=True, exist_ok=True)
 
-    accuracies: dict[str, list[float]] = run_ablations(
+    accuracies: dict[str, list[float]] = run_interaction_algorithm(
         model_config_dict=model_config_dict,
         mlp_path=config.mlp_path,
         hook_configs=config.hook_configs,

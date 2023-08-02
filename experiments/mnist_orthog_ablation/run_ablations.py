9087cf89b694ab27d125146a7e59844836609395
Dan Braun <dan@apolloresearch.ai>
2023-08-02 14:41:47 +0100

    Feature/rib rotation matrix (#31)
    Creates a functional, but untested script `experiments/mnist-rib/build_interaction_graph.py` which calculates the interaction rotations (Cl in the paper), but not yet the edge weights.
    
    * Rename interaction to rotation where necessary
    
    * Fix interaction matrix formula
    
    * Rename eigen info to gram eigen info
    
    * Add interaction rotation calcs with stub functions
    
    * Move data accumulation utils to own module
    
    * Add test for collect_gram_matrices
    
    * Test hook accumulation works with gram hook
    
    * Make collect_gram_matrices return the gram matrices
    
    * Convert to and from float64 for eigendecomposition
    
    * Remove deprected float conversion
    
    * Make eigendecomposition return eigenvectors as rows
    
    * Revert "Make eigendecomposition return eigenvectors as rows"
    
    This reverts commit 67086779c8de3f6e319566b77fd3b9354cab12aa.
    
    * WIP Add interaction rotation calculation
    
    * Use module_names instead of hook configs for orthog ablation
    
    * Fix plot ablations to work with exp_name param
    
    * Functional interaction rotation calculations
    
    * Rename pytest fixture
    
    * Update README.md
    
    * Fix einsum bug for Cl calc

--- experiments/mnist_orthog_ablation/run_ablations.py
+++ experiments/mnist_orthog_ablation/run_ablations.py
@@ -2,15 +2,20 @@
 
 The process is as follows:
     1. Load an MLP trained on MNIST and a test set of MNIST images.
-    2. Calculate gram matrices at the given hook points.
-    3. Calculate a rotation matrix at each hook point representing the operation of rotating to and
+    2. Calculate gram matrices at each node layer.
+    3. Calculate a rotation matrix at each node layer representing the operation of rotating to and
        from the partial eigenbasis of the gram matrix. The partial eigenbasis is equal to the entire
        eigenbasis with the zeroed out eigenvectors corresponding to the n smallest eigenvalues,
        where we let n range from 0 to the total number of eigenvalues (i.e. the dimension of the
        gram matrix).
-    4. Run the test set through the MLP, applying the rotations at each hook point, and calculate
+    4. Run the test set through the MLP, applying the rotations at each node layer, and calculate
        the resulting accuracy.
     5. Repeat steps 3 and 4 for a range of values of n, plotting the resulting accuracies.
+
+A node layer is positioned at the input of each module_name specified in the config. In this script,
+we don't create a node layer at the output of the final module, as ablating nodes in this layer
+is not useful.
+
 
 Usage:
     python run_ablations.py <path/to/yaml_config_file>
@@ -31,31 +36,19 @@ from torch import Tensor
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from tqdm import tqdm
-from typing_extensions import Literal
 
+from rib.data_accumulator import collect_gram_matrices
 from rib.hook_manager import Hook, HookedModel
 from rib.linalg import calc_rotation_matrix, eigendecompose
 from rib.log import logger
 from rib.models import MLP
-from rib.utils import (
-    REPO_ROOT,
-    eval_model_accuracy,
-    load_config,
-    run_dataset_through_model,
-)
-
-
-class HookConfig(BaseModel):
-    hook_name: str
-    module_name: str  # The module to hook into
-    hook_type: Literal["forward", "pre_forward"]
-    layer_size: int  # The size of the data at the hook point
+from rib.utils import REPO_ROOT, eval_model_accuracy, load_config
 
 
 class Config(BaseModel):
-    mlp_name: str
+    exp_name: str
     mlp_path: Path
-    hook_configs: list[HookConfig]
+    module_names: list[str]
 
 
 def load_mlp(config_dict: dict, mlp_path: Path) -> MLP:
@@ -82,7 +75,7 @@ def load_mnist_dataloader(train: bool = False, batch_size: int = 64) -> DataLoad
 
 def ablate_and_test(
     hooked_mlp: HookedModel,
-    hook_config: HookConfig,
+    module_name: str,
     test_loader: DataLoader,
     eigenvecs: Float[Tensor, "d_hidden d_hidden"],
     device: str,
@@ -91,6 +84,7 @@ def ablate_and_test(
 
     Args:
         hooked_mlp: The hooked model.
+        module_name: The name of the module whose inputs we want to rotate and ablate.
         hook_config: The config for the hook point.
         test_loader: The DataLoader for the test data.
         eigenvecs: A matrix whose columns are the eigenvectors of the gram matrix.
@@ -99,23 +93,21 @@ def ablate_and_test(
     Returns:
         A list of accuracies for each number of ablated vectors.
     """
-    assert hook_config.hook_type in ["forward", "pre_forward"]
-    rotate_hook_fn_name = "rotate_" + hook_config.hook_type + "_hook_fn"
 
     accuracies: list[float] = []
 
     # Iterate through possible number of ablated vectors.
     for n_ablated_vecs in tqdm(
-        range(hook_config.layer_size + 1),
-        total=len(range(hook_config.layer_size + 1)),
-        desc=f"Ablating {hook_config.module_name}",
+        range(len(eigenvecs) + 1),
+        total=len(range(len(eigenvecs) + 1)),
+        desc=f"Ablating {module_name}",
     ):
         rotation_matrix = calc_rotation_matrix(eigenvecs, n_ablated_vecs=n_ablated_vecs)
         rotation_hook = Hook(
-            name=hook_config.hook_name,
+            name=module_name,
             data_key="rotation",
-            fn_name=rotate_hook_fn_name,
-            module_name=hook_config.module_name,
+            fn_name="rotate_orthog_pre_forward_hook_fn",
+            module_name=module_name,
             fn_kwargs={"rotation_matrix": rotation_matrix},
         )
 
@@ -128,17 +120,18 @@ def ablate_and_test(
 
 
 def run_ablations(
-    model_config_dict: dict, mlp_path: Path, hook_configs: list[HookConfig]
+    model_config_dict: dict, mlp_path: Path, module_names: list[str]
 ) -> dict[str, list[float]]:
     """Rotate to and from orthogonal basis and compare accuracies with and without ablation.
 
     Args:
         model_config_dict: The config dictionary used for training the mlp.
         mlp_path: The path to the saved mlp.
-        hook_configs: Information about the hook points.
+        module_names: The names of the modules we want to build the graph around.
+
 
     Returns:
-        A dictionary mapping hook points to accuracy results.
+        A dictionary mapping node lyae to accuracy results.
     """
     device = "cuda" if torch.cuda.is_available() else "cpu"
     mlp = load_mlp(model_config_dict, mlp_path)
@@ -146,40 +139,23 @@ def run_ablations(
     mlp.to(device)
     hooked_mlp = HookedModel(mlp)
 
-    gram_hooks: list[Hook] = []
-    for hook_config in hook_configs:
-        assert hook_config.hook_type in ["forward", "pre_forward"]
-        gram_hook_fn_name = f"gram_{hook_config.hook_type}_hook_fn"
-        gram_hooks.append(
-            Hook(
-                name=hook_config.hook_name,
-                data_key="gram",
-                fn_name=gram_hook_fn_name,
-                module_name=hook_config.module_name,
-            )
-        )
-
     test_loader = load_mnist_dataloader(train=False, batch_size=512)
 
-    # Collect gram matrices
-    run_dataset_through_model(hooked_mlp, test_loader, gram_hooks, device=device)
-    len_dataset = len(test_loader.dataset)  # type: ignore
+    gram_matrices = collect_gram_matrices(
+        hooked_mlp, module_names, test_loader, device=device, collect_output_gram=False
+    )
 
     results: dict[str, list[float]] = {}
-    for hook_config in hook_configs:
-        # Scale the gram matrix by the number of samples in the dataset.
-        hooked_mlp.hooked_data[hook_config.hook_name]["gram"] = (
-            hooked_mlp.hooked_data[hook_config.hook_name]["gram"] / len_dataset
-        )
-        _, eigenvecs = eigendecompose(hooked_mlp.hooked_data[hook_config.hook_name]["gram"])
+    for module_name in module_names:
+        _, eigenvecs = eigendecompose(gram_matrices[module_name])
         accuracies: list[float] = ablate_and_test(
             hooked_mlp=hooked_mlp,
-            hook_config=hook_config,
+            module_name=module_name,
             test_loader=test_loader,
             eigenvecs=eigenvecs,
             device=device,
         )
-        results[hook_config.hook_name] = accuracies
+        results[module_name] = accuracies
 
     return results
 
@@ -191,7 +167,7 @@ def main(config_path_str: str) -> None:
     with open(config.mlp_path.parent / "config.yaml", "r") as f:
         model_config_dict = yaml.safe_load(f)
 
-    out_file = Path(__file__).parent / "out" / f"{config.mlp_name}_ablation_results.json"
+    out_file = Path(__file__).parent / "out" / f"{config.exp_name}_ablation_results.json"
     if out_file.exists():
         logger.error("Output file %s already exists. Exiting.", out_file)
         return
@@ -201,10 +177,10 @@ def main(config_path_str: str) -> None:
     accuracies: dict[str, list[float]] = run_ablations(
         model_config_dict=model_config_dict,
         mlp_path=config.mlp_path,
-        hook_configs=config.hook_configs,
+        module_names=config.module_names,
     )
     results = {
-        "mlp_name": config.mlp_name,
+        "exp_name": config.exp_name,
         "accuracies": accuracies,
     }
     with open(out_file, "w") as f:
---
+++ tests/test_data_accumulator.py
@@ -0,0 +1,44 @@
+import pytest
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+from rib.data_accumulator import collect_gram_matrices
+from rib.hook_manager import HookedModel
+
+
+def test_collect_gram_matrices():
+    """Test collect gram matrices function"""
+
+    torch.manual_seed(0)
+
+    class TestModel(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.linear = torch.nn.Linear(3, 3)
+            self.relu = torch.nn.ReLU()
+
+        def forward(self, x):
+            return self.relu(self.linear(x))
+
+    # Setup the model, data_loader, and HookConfig
+    hooked_mlp = HookedModel(TestModel())
+    data = torch.randn((30, 3))
+    targets = torch.ones(30)
+    data_loader = DataLoader(TensorDataset(data, targets), batch_size=13)
+
+    # Test with no module_names should raise assertion
+    with pytest.raises(AssertionError):
+        collect_gram_matrices(hooked_mlp, [], data_loader, "cpu")
+
+    module_names = ["linear", "relu"]
+    device = "cpu"
+
+    # Run collect_gram_matrices
+    gram_matrices = collect_gram_matrices(hooked_mlp, module_names, data_loader, device)
+
+    # Ensure hooked_mlp.hooked_data got removed after collect_gram_matrices
+    assert not hooked_mlp.hooked_data
+
+    for module_name in module_names:
+        # Ensure gram matrices are of correct size
+        assert gram_matrices[module_name].size() == torch.Size([3, 3])

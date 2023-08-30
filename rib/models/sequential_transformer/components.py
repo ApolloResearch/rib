fdac4b14e844282390ed8172e2d983535162f8bc
Avery Griffin <averyzanegriffin@gmail.com>
2023-08-30 18:02:47 +0100

    fix MLPIn, MLPAct, and MLPOut parameter initializations.
     remove all parameters from MLPAct. add biases to In and Out. removed redundant Win and Wout

--- rib/models/sequential_transformer/components.py
+++ rib/models/sequential_transformer/components.py
@@ -279,7 +279,7 @@ class MLPIn(nn.Module):
         super().__init__()
         self.cfg = cfg
         self.W_in = nn.Parameter(torch.empty(self.cfg.d_model, self.cfg.d_mlp, dtype=cfg.dtype))
-        self.W_out = nn.Parameter(torch.empty(self.cfg.d_mlp, self.cfg.d_model, dtype=cfg.dtype))
+        self.b_in = nn.Parameter(torch.empty(self.cfg.d_mlp, dtype=cfg.dtype))
 
     def forward(
         self,
@@ -295,8 +295,6 @@ class MLPAct(nn.Module):
     def __init__(self, cfg: SequentialTransformerConfig):
         super().__init__()
         self.cfg = cfg
-        self.W_in = nn.Parameter(torch.empty(self.cfg.d_model, self.cfg.d_mlp, dtype=cfg.dtype))
-        self.W_out = nn.Parameter(torch.empty(self.cfg.d_mlp, self.cfg.d_model, dtype=cfg.dtype))
 
         self.act_fn: Callable[[Float[Tensor, "... d_model"]], Tensor]
         if self.cfg.act_fn == "relu":
@@ -322,8 +320,8 @@ class MLPOut(nn.Module):
     def __init__(self, cfg: SequentialTransformerConfig):
         super().__init__()
         self.cfg = cfg
-        self.W_in = nn.Parameter(torch.empty(self.cfg.d_model, self.cfg.d_mlp, dtype=cfg.dtype))
         self.W_out = nn.Parameter(torch.empty(self.cfg.d_mlp, self.cfg.d_model, dtype=cfg.dtype))
+        self.b_out = nn.Parameter(torch.empty(self.cfg.d_model, dtype=cfg.dtype))
 
     def forward(
         self,

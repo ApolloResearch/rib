0a501646916771670c53893b0e5ff835616dfc8a
Avery Griffin <averyzanegriffin@gmail.com>
2023-08-30 18:04:29 +0100

    refactor transformer initialization to be flat. needed for state_dict() mapping

--- rib/models/sequential_transformer/transformer.py
+++ rib/models/sequential_transformer/transformer.py
@@ -2,7 +2,7 @@
 Defines a Transformer based on transformer lens but with a module hierarchy that allows for easier
 building of a RIB graph.
 """
-
+from collections import OrderedDict
 from typing import Type
 
 from jaxtyping import Int
@@ -78,39 +78,102 @@ class SequentialTransformer(nn.Module):
     def __init__(self, cfg: SequentialTransformerConfig, node_layers: list[str]):
         super().__init__()
         self.cfg = cfg
-
+        self.node_layers = node_layers
         assert len(node_layers) > 1, "Must have at least 2 node layers"
-        self.module_name_sections = self.create_module_name_sections(cfg.n_layers, node_layers)
 
-        if cfg.normalization_type == "LNPre":
-            ln_class = SeqLayerNormPre_Folded
-        elif cfg.normalization_type is None:
-            ln_class = nn.Identity
+        self.module_name_sections = None
+        self.sections = None
+        self.rename_state_dict_keys = True
+
+        if self.cfg.normalization_type == "LNPre":
+            self.ln_class = SeqLayerNormPre_Folded
+        elif self.cfg.normalization_type is None:
+            self.ln_class = nn.Identity
         else:
-            raise ValueError(f"Normalization type {cfg.normalization_type} not supported")
+            raise ValueError(f"Normalization type {self.cfg.normalization_type} not supported")
 
-        has_pre_section = node_layers[0] != "embed"
+        self.flattened_layers = self.initialize_flat_model()
+
+    def toggle_state_dict_renaming(self, flag: bool):
+        self.rename_state_dict_keys = flag
+
+    def state_dict(self, destination=None, prefix="", keep_vars=False) -> OrderedDict[str, Tensor]:
+        sd = super().state_dict(destination, prefix, keep_vars)
+
+        if not self.rename_state_dict_keys:
+            return sd
+
+        new_sd = OrderedDict()
+        for k, v in sd.items():
+            # Strip the undesired prefix and replace the underscore
+            new_k = k
+            if new_k.startswith("flattened_layers."):
+                new_k = new_k[len("flattened_layers.") :]
+            new_k = new_k.replace("blocks_", "blocks.")
+            new_k = new_k.replace("mlp_in", "mlp")
+            new_k = new_k.replace("mlp_out", "mlp")
+            new_sd[new_k] = v
+
+        return new_sd
+
+    def initialize_flat_model(self) -> nn.ModuleDict[str, nn.Module]:
+        main_dict = OrderedDict()
+
+        for module_name in SequentialTransformer.embed_module_names:
+            module_class = SEQUENTIAL_COMPONENT_REGISTRY[module_name]
+            module = module_class(self.cfg)
+            main_dict[module_name] = module
+
+        for block_num in range(self.cfg.n_layers):
+            block_dict = OrderedDict()
+            for module_name in SequentialTransformer.block_module_names:
+                module_class = (
+                    self.ln_class
+                    if module_name in ["ln1", "ln2"]
+                    else SEQUENTIAL_COMPONENT_REGISTRY[module_name]
+                )
+                module = module_class(self.cfg)
+                block_dict[module_name] = module
+            block_module_dict = nn.ModuleDict(block_dict)
+            main_dict[f"blocks_{block_num}"] = block_module_dict
+
+        unembed_module_class = SEQUENTIAL_COMPONENT_REGISTRY[
+            SequentialTransformer.unembed_module_name
+        ]
+        unembed_module = unembed_module_class(self.cfg)
+        main_dict[SequentialTransformer.unembed_module_name] = unembed_module
+
+        return nn.ModuleDict(main_dict)
+
+    # TODO refactor given initialize_flat_model
+    def structure_graph(self):
+        self.module_name_sections = self.create_module_name_sections(
+            self.cfg.n_layers, self.node_layers
+        )
+
+        has_pre_section = self.node_layers[0] != "embed"
         # Initialize the modules, creating a ModuleList of Sequential modules for each graph section
         sections: dict[str, MultiSequential] = {}
         # If has_pre_section, we need to start at -1 because the first section will be the
         # pre-section which should not be part of the graph
         for i, module_names in enumerate(self.module_name_sections, -1 if has_pre_section else 0):
-            section_name = "pre" if node_layers[0] != "embed" and i == -1 else f"section_{i}"
+            section_name = "pre" if self.node_layers[0] != "embed" and i == -1 else f"section_{i}"
 
             module_section: list[nn.Module] = []
             for module_name in module_names:
                 module_type = module_name.split(".")[0]
                 module_class: Type[nn.Module]
                 if module_type in ["ln1", "ln2"]:
-                    module_class = ln_class
+                    module_class = self.ln_class
                 else:
                     module_class = SEQUENTIAL_COMPONENT_REGISTRY[module_type]
-                module = module_class(cfg)
+                module = module_class(self.cfg)
                 module_section.append(module)
             sections[section_name] = MultiSequential(*module_section)
         self.sections: nn.ModuleDict = nn.ModuleDict(sections)
 
     @staticmethod
+    # TODO refactor given initialize_flat_model
     def create_module_name_sections(n_blocks: int, node_layers: list[str]) -> list[list[str]]:
         """Create ordered groups of module names.
 

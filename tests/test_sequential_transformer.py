471ecc041475cc8e711ae26c0639fa4b840841dc
Dan Braun <dan@apolloresearch.ai>
2023-09-12 16:58:10 +0000

    Use float64 to avoid floating point errors

--- tests/test_sequential_transformer.py
+++ tests/test_sequential_transformer.py
@@ -44,7 +44,10 @@ def _folded_bias_comparison(
 
 
 def test_modular_arithmetic_folded_bias() -> None:
-    """Test that the folded bias trick works for a model used for modular arithmetic."""
+    """Test that the folded bias trick works for a model used for modular arithmetic.
+
+    Floating point errors heavily accumulate here with float32 or less, so we use float64.
+    """
     set_seed(42)
     cfg = {
         "n_layers": 2,
@@ -56,13 +59,18 @@ def test_modular_arithmetic_folded_bias() -> None:
         "n_ctx": 3,
         "act_fn": "gelu_new",
         "normalization_type": "LNPre",
+        "dtype": torch.float64,
     }
+    # Need atols to be larger for lower precision dtypes (1e3 for bfloat16, 1e-2 for float32)
+    atol = 1e-8
+
     tlens_cfg = HookedTransformerConfig.from_dict(cfg)
     cfg = SequentialTransformerConfig(**asdict(tlens_cfg))
 
     node_layers = ["attn.0", "mlp_act.0"]
 
     model_raw = SequentialTransformer(cfg, node_layers)
+    model_raw.eval()
     # Manually set all bias vectors to random values (to avoid the default of 0)
 
     seq_tf_keys = list(model_raw.state_dict().keys())
@@ -76,16 +84,26 @@ def test_modular_arithmetic_folded_bias() -> None:
     model_folded = SequentialTransformer(cfg, node_layers)
     model_folded.load_state_dict(model_raw.state_dict())
     model_folded.fold_bias()
-    _folded_bias_comparison(model_raw, model_folded)
+    model_folded.eval()
+
+    _folded_bias_comparison(model_raw, model_folded, atol=atol)
 
 
 @pytest.mark.slow()
 def test_gpt2_folded_bias() -> None:
-    """Test that the folded bias trick works for GPT2."""
+    """Test that the folded bias trick works for GPT2.
+
+    Uses float64 to avoid floating point errors.
+    """
     set_seed(42)
     model_raw = HookedTransformer.from_pretrained("gpt2")
     model_folded = HookedTransformer(cfg=model_raw.cfg, tokenizer=model_raw.tokenizer)
     model_folded.load_state_dict(model_raw.state_dict())
     model_fold_bias(model_folded)
-    # Fails for atol=1e-4, not investigating further for now but could be an issue
-    _folded_bias_comparison(model_raw, model_folded, atol=1e-3)
+
+    model_raw.eval()
+    model_folded.eval()
+    model_raw.to(torch.float64)
+    model_folded.to(torch.float64)
+    # Need to use atol=1e-3 if you want to test float32.
+    _folded_bias_comparison(model_raw, model_folded, atol=1e-6)

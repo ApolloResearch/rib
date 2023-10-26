b1f18c38dba1d3f29ce67399a2e56f81cc9991f5
Stefan Heimersheim <stefan@apolloresearch.ai>
2023-10-26 10:47:26 +0000

    Load dataset

--- experiments/train_modular_arithmetic/play_mod_arithmetic.py
+++ experiments/train_modular_arithmetic/play_mod_arithmetic.py
@@ -1,11 +1,68 @@
-import torch
+# %%
+import json
+from datetime import datetime
+from pathlib import Path
+from typing import Optional
 
+import fire
+import torch
+import torch.optim as optim
+import wandb
+from pydantic import BaseModel
+from torch import nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+from transformer_lens import HookedTransformer, HookedTransformerConfig
+
+from rib.data import ModularArithmeticDatasetConfig
 from rib.hook_manager import HookedModel
 from rib.loader import create_data_loader, load_dataset, load_sequential_transformer
+from rib.log import logger
+from rib.models.utils import save_model
 from rib.types import TORCH_DTYPES
+from rib.utils import load_config, set_seed
+
+torch.set_grad_enabled(False)
+
+
+class ModelConfig(BaseModel):
+    n_layers: int
+    d_model: int
+    d_head: int
+    n_heads: int
+    d_mlp: int
+    d_vocab: int
+    n_ctx: int
+    act_fn: str
+    normalization_type: Optional[str]
+
+
+class TrainConfig(BaseModel):
+    learning_rate: float
+    batch_size: int  # Set to max(batch_size, <number of samples in dataset>)
+    epochs: int
+    eval_every_n_epochs: int
+    save_dir: Optional[Path] = None
+    save_every_n_epochs: Optional[int] = None
+
+
+class WandbConfig(BaseModel):
+    project: str
+    entity: Optional[str]
+
+
+class Config(BaseModel):
+    seed: int
+    model: ModelConfig
+    train: TrainConfig
+    dataset: ModularArithmeticDatasetConfig
+    wandb: Optional[WandbConfig]
+
+
+# %%
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
 dtype = TORCH_DTYPES["float32"]
 
 seq_model, tlens_cfg_dict = load_sequential_transformer(
@@ -18,9 +75,27 @@ seq_model, tlens_cfg_dict = load_sequential_transformer(
     device=device,
 )
 
+# %%
+
 seq_model.eval()
 seq_model.to(device=torch.device(device), dtype=dtype)
 seq_model.fold_bias()
 hooked_model = HookedModel(seq_model)
 
-hooked_model(torch.tensor([3, 3, 4]))
+
+# %%
+config_path = Path("mod_arithmetic_config.yaml")
+config = load_config(config_path, config_model=Config)
+
+datasets = load_dataset(dataset_config=config.dataset, return_set=config.dataset.return_set)
+train_loader, test_loader = create_data_loader(datasets, shuffle=True, batch_size=11, seed=0)
+
+train_loader.dataset[0:10]
+
+# %%
+
+logits = hooked_model(torch.tensor([3, 3, 113]))[0]
+
+logits.argmax(dim=-1)
+
+# %%

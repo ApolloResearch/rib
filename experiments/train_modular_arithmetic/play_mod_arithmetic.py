dcd1c7d32399966339de6ad98ae84f6add9a2284
Stefan Heimersheim <stefan@apolloresearch.ai>
2023-10-30 14:34:00 +0000

    Solved confusion re rotation matrix inputs and outputs, refactored code

--- experiments/train_modular_arithmetic/play_mod_arithmetic.py
+++ experiments/train_modular_arithmetic/play_mod_arithmetic.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 import einops
 import fire
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.optim as optim
@@ -90,7 +91,25 @@ seq_model.to(device=torch.device(device), dtype=dtype)
 seq_model.fold_bias()
 hooked_model = HookedModel(seq_model)
 
+logits = hooked_model(torch.tensor([3, 5, 113]))[0]
+assert logits.argmax(dim=-1).item() == 8
+
+logits, cache = hooked_model.run_with_cache(torch.tensor([3, 8, 113]))
+
 print(hooked_model)
+
+for key, value in cache.items():
+    for index, stream in enumerate(value["acts"]):
+        print(key, index, cache[key]["acts"][index].shape)
+
+# %%
+config_path = Path("mod_arithmetic_config.yaml")
+config = load_config(config_path, config_model=Config)
+
+datasets = load_dataset(dataset_config=config.dataset, return_set=config.dataset.return_set)
+train_loader, test_loader = create_data_loader(datasets, shuffle=True, batch_size=11, seed=0)
+
+print(train_loader.dataset[0:10])
 
 # %%
 
@@ -103,92 +122,123 @@ print(hooked_model)
 #     Maximum activating dataset examples for these
 
 # %%
-config_path = Path("mod_arithmetic_config.yaml")
-config = load_config(config_path, config_model=Config)
-
-datasets = load_dataset(dataset_config=config.dataset, return_set=config.dataset.return_set)
-train_loader, test_loader = create_data_loader(datasets, shuffle=True, batch_size=11, seed=0)
-
-print(train_loader.dataset[0:10])  #
-
-# %%
-
-logits = hooked_model(torch.tensor([3, 3, 113]))[0]
-
-logits.argmax(dim=-1)
-
-# %%
-
-logits, cache = hooked_model.run_with_cache(torch.tensor([3, 3, 113]))
-
-for key, value in cache.items():
-    for index, stream in enumerate(value["acts"]):
-        print(key, index, cache[key]["acts"][index].shape)
 
 
-# %%
+def get_normal_activations(
+    hooked_model=hooked_model, section="sections.section_1.2", sizes=(129, 513), p=113
+):
+    return_acts = []
+    for size in sizes:
+        return_acts.append(torch.empty([p, p, size]))
 
-# Plot activations of MLP Act section 1.2
-# Store all in tensor
-resid_acts = torch.empty([113, 113, 129])
-mlp_acts = torch.empty([113, 113, 513])
-# Iterate all inputs
-p = 113
-for x in tqdm(range(p)):
-    for y in range(p):
-        logits, cache = hooked_model.run_with_cache(torch.tensor([x, y, p]))
-        # Get activations
-        acts = cache["sections.section_1.2"]["acts"]
-        resid_acts[x, y] = acts[0][0]  # stream, batch
-        mlp_acts[x, y] = acts[1][0]
+    for x in tqdm(range(p)):
+        for y in range(p):
+            batch_index = 0
+            _, cache = hooked_model.run_with_cache(torch.tensor([x, y, p]))
+            # Get activations
+            acts = cache[section]["acts"]
+            assert len(acts) == len(sizes)
+            for i, act in enumerate(acts):
+                assert act[batch_index].shape[0] == sizes[i], f"{act.shape[0]} != {sizes[i]}"
+                return_acts[i][x, y] = act[batch_index]
+    return tuple(return_acts)
+
 
 # %%
-
-# Now collect RIB features
-interaction_graph_path = "/mnt/ssd-apollo/dan/RIB/rib/experiments/lm_rib_build/out/modular_arithmetic_interaction_graph.pt"
-interaction_graph_info = torch.load(interaction_graph_path)
-logits_node_layer = interaction_graph_info["config"]["node_layers"]
-node_layers = interaction_graph_info["config"]["node_layers"]
-ablation_node_layers = node_layers if logits_node_layer else node_layers[:-1]
-ablation_type = "rib"
-
-basis_matrices = load_basis_matrices(
-    interaction_graph_info=interaction_graph_info,
-    ablation_node_layers=ablation_node_layers,
-    ablation_type=ablation_type,
-    dtype=dtype,
-    device=device,
+sec_1_1_resid_acts, sec_1_1_mlp_pre_acts = get_normal_activations(
+    section="sections.section_1.1", sizes=(129, 513)
 )
+sec_1_2_resid_acts, sec_1_2_mlp_post_acts = get_normal_activations(
+    section="sections.section_1.2", sizes=(129, 513)
+)
+assert torch.allclose(sec_1_1_resid_acts, sec_1_2_resid_acts)
+assert torch.allclose(torch.relu(sec_1_1_mlp_pre_acts), sec_1_2_mlp_post_acts)
 
-node_index = np.where("mlp_out.0" == np.array(node_layers))[0][0]
-target = "sections.section_1.2"
+sec_2_1_resid_acts = get_normal_activations(section="sections.section_2.1", sizes=(129,))[0]
+# %%
 
-# Turns out it's 556 cause basis matrices are 556 rather than 642 (I assume trunc'd zeros)
-rib_acts = torch.empty([113, 113, basis_matrices[node_index][0].shape[1]])
-for x in tqdm(range(p)):
-    for y in range(p):
-        logits, cache = hooked_model.run_with_cache(torch.tensor([x, y, p]))
-        # Get activations
-        acts = cache[target]["acts"]
-        acts = torch.cat([acts[0][0], acts[1][0]], dim=-1)
-        transformed_acts = einops.einsum(acts, basis_matrices[node_index][0], "act, act rib -> rib")
-        rib_acts[x, y] = transformed_acts
 
-# def load_basis_matrices(
-#     interaction_graph_info: dict,
-#     ablation_node_layers: list[str],
-#     ablation_type: Literal["rib", "orthogonal"],
-#     dtype: torch.dtype,
-#     device: str,
-# ) -> list[tuple[BasisVecs, BasisVecsPinv]]:
+def plot_activations(acts, title="Default title", nrows=3, ncols=2):
+    fig, axes = plt.subplots(nrows, ncols, constrained_layout=True, figsize=(8, 10))
+    fig.suptitle(title)
+    for i, axs in enumerate(axes):
+        for j, ax in enumerate(axs):
+            ax.set_title(f"({i * ncols + j})")
+            im = ax.imshow(acts[:, :, i * ncols + j].numpy())
+            # Color
+            fig.colorbar(im, ax=ax)
 
-#
-# for x in tqdm(range(p)):
-#     for y in range(p):
-#   - ln1.0
-#   - ln2.0
-#   - mlp_out.0
-#   - unembed
+
+# plot_activations(sec_1_1_resid_acts, title="Residual activations section 1.1")
+# plot_activations(sec_1_1_mlp_pre_acts, title="MLP pre-activations section 1.1")
+# plot_activations(sec_1_2_resid_acts, title="Residual activations section 1.2")
+# plot_activations(sec_1_2_mlp_post_acts, title="MLP post-activations section 1.2")
+plot_activations(sec_2_1_resid_acts, title="Residual activations section 2.1")
+
+
+# %%
+
+
+def connect_RIB_acts(
+    hooked_model=hooked_model,
+    section="sections.section_0.2",
+    p=113,
+    interaction_graph_path="/mnt/ssd-apollo/dan/RIB/rib/experiments/lm_rib_build/out/modular_arithmetic_interaction_graph.pt",
+    ablation_type="rib",
+):
+    # node_layer_dict = {
+    #     "ln1.0": "sections.section_0.0",
+    #     "ln2.0": "sections.section_1.0",
+    #     "mlp_out.0": "sections.section_2.0",
+    #     "unembed": "sections.section_2.2",
+    # }
+    # Issue: Make a mapping function given some model
+    # Issue: run_with_cache give inputs or outputs
+    node_layer_dict = {
+        "sections.section_pre.2": "ln1.0",
+        "sections.section_0.2": "ln2.0",
+        "sections.section_1.2": "mlp_out.0",
+        "sections.section_2.2": "unembed",
+    }
+
+    interaction_graph_info = torch.load(interaction_graph_path)
+    logits_node_layer = interaction_graph_info["config"]["node_layers"]
+    node_layers = interaction_graph_info["config"]["node_layers"]
+    ablation_node_layers = node_layers if logits_node_layer else node_layers[:-1]
+
+    basis_matrices = load_basis_matrices(
+        interaction_graph_info=interaction_graph_info,
+        ablation_node_layers=ablation_node_layers,
+        ablation_type=ablation_type,
+        dtype=dtype,
+        device=device,
+    )
+
+    node_layer = node_layer_dict[section]
+    node_index = np.where(node_layer == np.array(node_layers))[0][0]
+    assert (
+        len(basis_matrices[node_index]) == 2
+    ), f"This should be C and C_inv but contains {len(basis_matrices[node_index])} elements"
+    basis_matrices_index = 0  # get C rather than C_inv
+    # basis_matrices[node_index][0].shape = (act_dim_concat, rib_dim) with rib_dim <= act_dim
+    print("Rotation matrix shape", basis_matrices[node_index][basis_matrices_index].shape)
+    rib_return_acts = torch.empty([p, p, basis_matrices[node_index][basis_matrices_index].shape[1]])
+    for x in tqdm(range(p)):
+        for y in range(p):
+            batch_index = 0
+            _, cache = hooked_model.run_with_cache(torch.tensor([x, y, p]))
+            acts = cache[section]["acts"]
+            # print("Shapes", [act.shape for act in acts])
+            acts = torch.cat([acts[i][batch_index] for i in range(len(acts))], dim=-1)
+            transformed_acts = einops.einsum(
+                acts, basis_matrices[node_index][basis_matrices_index], "act, act rib -> rib"
+            )
+            rib_return_acts[x, y] = transformed_acts
+    return rib_return_acts
+
+
+rib_acts_out = connect_RIB_acts()
+
 
 # %%
 
@@ -201,14 +251,14 @@ fig, axes = plt.subplots(nrows, ncols, constrained_layout=True, figsize=(6, 8))
 for i, axs in enumerate(axes):
     for j, ax in enumerate(axs):
         ax.set_title(f"({i * ncols + j})")
-        im = ax.imshow(mlp_acts[:, :, i * ncols + j].numpy())
+        im = ax.imshow(sec_1_1_mlp_pre_acts[:, :, i * ncols + j].numpy())
         fig.colorbar(im, ax=ax)
 
 fig, axes = plt.subplots(nrows, ncols, constrained_layout=True, figsize=(6, 8))
 for i, axs in enumerate(axes):
     for j, ax in enumerate(axs):
         ax.set_title(f"({i * ncols + j})")
-        im = ax.imshow(resid_acts[:, :, i * ncols + j].numpy())
+        im = ax.imshow(sec_1_1_resid_acts[:, :, i * ncols + j].numpy())
         fig.colorbar(im, ax=ax)
 
 # %%
@@ -217,7 +267,7 @@ fig, axes = plt.subplots(nrows, ncols, constrained_layout=True, figsize=(8, 10))
 for i, axs in enumerate(axes):
     for j, ax in enumerate(axs):
         ax.set_title(f"({i * ncols + j})")
-        im = ax.imshow(rib_acts[:, :, i * ncols + j].numpy())
+        im = ax.imshow(rib_acts_out[:, :, i * ncols + j].numpy())
         # Color
         fig.colorbar(im, ax=ax)
 

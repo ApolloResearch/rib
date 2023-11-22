c2f1290ad401163c03b8337ba7d5c9a8d3324984
Stefan Heimersheim <stefan@apolloresearch.ai>
2023-11-22 18:08:22 +0000

    cos phase plotting script

--- experiments/interp_modular_arithmetic/plotting.py
+++ experiments/interp_modular_arithmetic/plotting.py
@@ -8,6 +8,108 @@ import torch
 from matplotlib.colors import LogNorm, Normalize, SymLogNorm
 
 
+def fft_plot_cos_phase_1d(acts, rtol=1e-4):
+    r"""
+    Plot the cos phase of a 1D FFT data. The input to the FFT is assumed to be real-valued.
+
+    Derivation for why we can express the FFT terms \tilde A_f e^(i2π/113 f x) as
+    A_f cos(2π/113 f x + φ), for real-valued input:
+
+        1. Convert from f=0...112 to f=-56...56 since f=57...112 correspond to f=-56...-1),
+           the difference is a expoent shift by 2π.
+
+        2. Convert to cos and sin terms, e^(i2π/113 f x) = cos(2π/113 f x) + i sin(2π/113 f x)
+
+        3. Combine pairs of negative and positive frequencies:
+           (A_f + A_-f) cos(2π/113 f x) + (A_f - A_-f) i sin(2π/113 f x)
+
+        4. Use that the ampltiudes of a real-valued FFT are complex conjugates A_f = conj(A_-f).
+           (A_f + A_-f) = 2 Re(A_f) and (A_f - A_-f) i = - 2 Im(A_f)
+
+        5. Finally combine cos and sin terms using "harmonic addition"
+           (https://en.wikipedia.org/wiki/List_of_trigonometric_identities#Sine_and_cosine):
+           a*cos(x) + b*sin(x) = c*cos(x+phi) with c=sign(a)*sqrt(a^2+b^2) and phi=arctan(-b/a)
+           or a*cos(x) + b*sin(x) = c*cos(x+phi) with c=sqrt(a^2+b^2) and phi=-atan2(b, a).
+
+           Each f term is sqrt(Re(2A_f)^2 + Im(2A_f)^2) cos(2π/113 f x -atan2(Im(-A_f), Re(A_f)))
+           or simply 2*|A_f| cos(2π/113 f x + atan2(Im(A_f), Re(A_f))).
+
+    Args:
+        acts: nD tensor of activations, complex. 0th dimension assumed to be frequency.
+
+    Returns:
+        fig, ax: matplotlib figure and axis objects.
+    """
+    # Calculate the FFT
+    n_freqs = acts.shape[0]
+    assert n_freqs % 2 == 1, f"n_freqs={n_freqs} must be odd"
+
+    # Apply fftshift, makes thinking about the frequencies easier
+    fftshift_freqs = torch.fft.fftshift(torch.fft.fftfreq(n_freqs))
+    fftshift_amplitudes = torch.fft.fftshift(acts)
+
+    # Central frequency (constant)
+    i_center = n_freqs // 2
+    assert fftshift_freqs[i_center] == 0, f"fftshift_freqs[center] = {fftshift_freqs[i_center]}"
+
+    # Collect the frequencies and compute the amplitudes and phases
+    freq_labels = []
+    freq_amplitudes = []
+    freq_phases = []
+    for i in np.arange(n_freqs // 2, n_freqs):
+        f = fftshift_freqs[i]
+        if i == i_center:
+            a = fftshift_amplitudes[i]
+            assert f == 0, "central frequency must be 0"
+            assert torch.all(torch.arctan(a.imag / a.real) < rtol), "central frequency must be real"
+            freq_labels.append("const")
+            freq_amplitudes.append(a.real)
+            freq_phases.append(torch.zeros_like(a.real))
+        else:
+            assert f > 0, f"should be iterating over positive frequencies but f={f}"
+            i_pos = i
+            i_neg = i_center - (i - i_center)
+            a_pos = fftshift_amplitudes[i_pos]
+            a_neg = fftshift_amplitudes[i_neg]
+            # Assert complex conjugate of amplitudes (due to real input)
+            assert torch.allclose(
+                a_pos, torch.conj(a_neg), rtol=rtol
+            ), "amplitude pairs must be complex conjugates (real input?)"
+            assert (n_freqs * f + 0.5) % 1 - 0.5 < rtol, f"{n_freqs}*f={n_freqs*f} must be integer"
+            freq_labels.append(f"cos{n_freqs*f:.0f}")
+            freq_amplitudes.append(2 * torch.sqrt(a_pos.real**2 + a_pos.imag**2))
+            freq_phases.append(-torch.atan2(a_pos.imag, a_pos.real))
+
+    # Plot the amplitudes and phases
+    freq_labels = np.array(freq_labels)
+    freq_labels_ticks = torch.arange(len(freq_labels))
+    freq_amplitudes = np.array(freq_amplitudes)
+    freq_phases = np.array(freq_phases)
+    fig, ax = plt.subplots(figsize=(12, 3), constrained_layout=True)
+    freq_labels_ticks_nD = (
+        freq_labels_ticks
+        if acts.ndim == 1
+        else torch.repeat_interleave(freq_labels_ticks, acts.shape[1])
+    )
+    im = ax.scatter(
+        freq_labels_ticks_nD,
+        freq_amplitudes,
+        c=freq_phases,
+        cmap="hsv",
+        vmin=-np.pi,
+        vmax=np.pi,
+        s=10,
+    )
+    ax.plot(freq_labels_ticks, freq_amplitudes, lw=0.5)
+    ax.set_xticks(np.arange(len(freq_labels)))
+    ax.set_xticklabels(freq_labels, rotation=-90)
+    ax.set_ylabel("Amplitude A")
+    ax.set_xlabel("Each term A cos f = A*cos(2π/113 f + φ)")
+    cbar = fig.colorbar(im, ax=ax, label="Phase φ")
+    ax.grid(color="grey", linestyle="--", alpha=0.3)
+    return fig, ax
+
+
 def annotated_fft_line_plot(
     acts,
     ax=None,

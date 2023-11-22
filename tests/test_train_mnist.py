abff27072b8db7091606d63e1ec216a22c547b9f
Nix Goldowsky-Dill <nix@apolloresearch.ai>
2023-11-22 14:42:34 +0000

    clean test train modadd

--- tests/test_train_mnist.py
+++ tests/test_train_mnist.py
@@ -1,4 +1,4 @@
-"""Run the mnist train script with a mock config and check that accuracy is > 95%.
+"""Run the mnist train script and checks that accuracy is > 95%.
 """
 import pytest
 import yaml
@@ -6,7 +6,7 @@ import yaml
 from experiments.train_mnist.run_train_mnist import Config
 from experiments.train_mnist.run_train_mnist import main as train_main
 
-MOCK_CONFIG = """
+CONFIG_STR = """
 seed: 0
 model:
   hidden_sizes: [30, 30]
@@ -25,12 +25,9 @@ wandb: null
 
 @pytest.mark.slow
 def test_main_accuracy():
-    """Test that the accuracy of the model is > 95%.
+    """Test that the accuracy of the model is > 95%."""
 
-    We don't use a context manager here because windows doesn't support opening temp files more than once.
-    """
-
-    config_dict = yaml.safe_load(MOCK_CONFIG)
+    config_dict = yaml.safe_load(CONFIG_STR)
     config = Config(**config_dict)
     accuracy = train_main(config)
     assert accuracy > 90.0

a88c36d10d350695268fac818d4b14d3116e0d09
Jake Mendel <jake@apolloresearch.ai>
2023-11-28 12:47:25 +0000

    more descriptive assert message in collect_interaction_edges

--- rib/data_accumulator.py
+++ rib/data_accumulator.py
@@ -228,7 +228,7 @@ def collect_interaction_edges(
     assert hooked_model.model.has_folded_bias, "Biases must be folded in to calculate edges."
 
     edge_modules = section_names if Cs[-1].node_layer_name == "output" else section_names[:-1]
-    assert len(edge_modules) == len(Cs) - 1, "Number of edge modules not the same as Cs - 1."
+    assert len(edge_modules) == len(Cs) - 1, f"Number of edge modules not the same as Cs - 1. Num edge modules: {len(edge_modules)}, Cs - 1: {len(Cs) - 1}"
 
     logger.info("Collecting edges for node layers: %s", [C.node_layer_name for C in Cs[:-1]])
     edge_hooks: list[Hook] = []

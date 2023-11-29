dab4c89170e223e0cc634a3b2c011ebbdb8e4160
nix-apollo <150139798+nix-apollo@users.noreply.github.com>
2023-11-29 16:06:03 +0000

    Add n_ctx to config (#209)
    * add n_ctx to config
    
    * Add test showing equivalent attn_scores for pythia
    
    * Place attn_scores_pre_forward_hook in hook_fns.py
    
    * Add tests comparing logits for different n_ctx
    
    * Set IGNORE to -1e20 in pythia test
    
    * Run n_ctx tests in float64
    
    * Fix typo and remove unneeded TODO comment
    
    * Rename attn_pattern to attn_scores
    
    * Check that n_ctx <= model_n_ctx
    
    * Remove unused import and fix type hint
    
    * Delete stale gpt2 config
    
    * Make model_n_ctx the default context length
    
    * Remove manual setting of IGNORE in test
    
    ---------
    
    Co-authored-by: Dan Braun <dan@apolloresearch.ai>

--- tests/test_model_utils.py
+++ tests/test_model_utils.py
@@ -2,9 +2,15 @@ from typing import TypeVar
 
 import pytest
 import torch
+from transformers import AutoTokenizer
 
+from rib.hook_fns import attn_scores_pre_forward_hook
+from rib.hook_manager import Hook, HookedModel
+from rib.loader import load_sequential_transformer
 from rib.models import SequentialTransformer, SequentialTransformerConfig
+from rib.models.sequential_transformer.transformer import MultiSequential
 from rib.models.utils import create_list_partitions
+from rib.utils import set_seed
 
 T = TypeVar("T")
 
@@ -213,3 +219,96 @@ def test_validate_node_layers_valid(node_layers: list[str], module_ids: list[str
 def test_validate_node_layers_invalid(node_layers: list[str], module_ids: list[str]):
     with pytest.raises(ValueError):
         SequentialTransformer.validate_node_layers(node_layers, module_ids)
+
+
+@pytest.mark.slow()
+def test_n_ctx_pythia():
+    """Test that varying n_ctx produces the same attention scores and outputs for pythia-14m.
+
+    Note that this test doesn't test any particular feature that we've implemented, but rather
+    that pythia in SequentialTransformer will work regardless of the value of n_ctx.
+
+    Process is a follows:
+    1. Initialize a pythia-14m model.
+    2. Create a fake data sample of length short_n_ctx.
+    3. Add random tokens to the fake data sample so that it is length long_n_ctx=2048.
+    4. Apply a pre-forward hook to attn_out.0 to calculate and save the attention scores.
+    5. Assert that the attention scores are the same for the first short_n_ctx positions.
+    6. Assert that the model outputs are the same for the first short_n_ctx positions.
+    """
+    set_seed(0)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    dtype = torch.float64
+    atol = 0  # Works with atol=0 for float64 and atol=1e-6 for float32
+    module_id = "attn_out.0"
+    batch_size = 2
+    short_n_ctx = 20
+    long_n_ctx = 2048
+
+    seq_model, _ = load_sequential_transformer(
+        node_layers=[module_id],
+        last_pos_module_type=None,
+        tlens_pretrained="pythia-14m",
+        tlens_model_path=None,
+        fold_bias=True,
+        dtype=dtype,
+        device=device,
+    )
+    seq_model.eval()
+
+    # We only care about the first module in section_0, so remove the rest
+    seq_model.sections.section_0 = MultiSequential(seq_model.sections.section_0[0])
+
+    hooked_model = HookedModel(seq_model)
+
+    # Create a fake data sample of length short_n_ctx
+    input_ids_short = torch.randint(
+        0, seq_model.cfg.d_vocab, (batch_size, short_n_ctx), device=device
+    )
+    # Add extra tokens to the fake data sample so that it is length n_ctx=2048
+    input_ids_long = torch.cat(
+        [
+            input_ids_short,
+            torch.randint(
+                0, seq_model.cfg.d_vocab, (batch_size, long_n_ctx - short_n_ctx), device=device
+            ),
+        ],
+        dim=1,
+    )
+
+    # Add a pre-forward hook to attn_out.0 and save the attention scores
+    hook = Hook(
+        name="pre_forward_attn_out.0",
+        data_key="attn_scores",
+        fn=attn_scores_pre_forward_hook,
+        module_name=seq_model.module_id_to_section_id[module_id],
+    )
+    with torch.inference_mode():
+        # Ran short_ctx example through model
+        out_short = hooked_model(input_ids_short, hooks=[hook])
+
+    attn_scores = hooked_model.hooked_data["pre_forward_attn_out.0"]["attn_scores"].detach().clone()
+
+    with torch.inference_mode():
+        # Ran long_ctx example through model
+        out_long = hooked_model(input_ids_long, hooks=[hook])
+
+    # Collect the attention scores for the first short_n_ctx positions
+    attn_scores_long = (
+        hooked_model.hooked_data["pre_forward_attn_out.0"]["attn_scores"][
+            ..., :short_n_ctx, :short_n_ctx
+        ]
+        .detach()
+        .clone()
+    )
+
+    assert torch.allclose(attn_scores, attn_scores_long, atol=atol), (
+        f"Attention scores for short_n_ctx={short_n_ctx} and long_n_ctx={long_n_ctx} are not equal."
+        f"Max difference: {torch.max(torch.abs(attn_scores - attn_scores_long))}"
+    )
+
+    # Check that the outputs for the first short_n_ctx positions are the same
+    assert torch.allclose(out_short[0], out_long[0][:, :short_n_ctx, :], atol=atol), (
+        f"Model output for short_n_ctx={short_n_ctx} and long_n_ctx={long_n_ctx} are not equal."
+        f"Max difference: {torch.max(torch.abs(out_short[0] - out_long[0][..., :short_n_ctx, :]))}"
+    )

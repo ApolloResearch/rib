30d2786b2eb7a24cdf7b3be0d0bc290ebc7d6686
nix-apollo <150139798+nix-apollo@users.noreply.github.com>
2023-12-04 10:41:16 +0000

    Add svd basis (#239)
    * add svd basis
    
    * remove stray config arg

--- experiments/lm_rib_build/run_lm_rib_build.py
+++ experiments/lm_rib_build/run_lm_rib_build.py
@@ -137,9 +137,10 @@ class Config(BaseModel):
         description="The type of evaluation to perform on the model before building the graph."
         "If None, skip evaluation.",
     )
-    basis_formula: Literal["(1-alpha)^2", "(1-0)*alpha"] = Field(
+    basis_formula: Literal["(1-alpha)^2", "(1-0)*alpha", "svd"] = Field(
         "(1-0)*alpha",
-        description="The integrated gradient formula to use to calculate the basis.",
+        description="The integrated gradient formula to use to calculate the basis. If 'svd', will"
+        "use Us as Cs, giving the eigendecomposition of the gram matrix.",
     )
     edge_formula: Literal["functional", "squared"] = Field(
         "functional",

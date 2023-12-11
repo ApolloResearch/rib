4e93469852062df10c7d40bb5fdb3154651185fb
Nix Goldowsky-Dill <nix@apolloresearch.ai>
2023-12-11 18:00:24 +0000

    add test for collect means

--- tests/test_data_accumulator.py
+++ tests/test_data_accumulator.py
@@ -2,8 +2,9 @@ import pytest
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
-from rib.data_accumulator import collect_gram_matrices
+from rib.data_accumulator import collect_dataset_means, collect_gram_matrices
 from rib.hook_manager import HookedModel
+from rib.models.mlp import MLP, MLPConfig
 from rib.utils import set_seed
 
 
@@ -46,3 +47,45 @@ def test_collect_gram_matrices():
     for module_name in module_names:
         # Ensure gram matrices are of correct size
         assert gram_matrices[module_name].size() == torch.Size([3, 3])
+
+
+def test_collect_dataset_means():
+    """Test collect dataset means function"""
+
+    set_seed(0)
+    dtype = torch.float32
+    device = "cpu"
+
+    config = MLPConfig(
+        input_size=3,
+        output_size=1,
+        hidden_sizes=[5],
+        activation_fn="relu",
+    )
+    mlp = MLP(config)
+    b0 = mlp.layers[0].b[:]
+    mlp.fold_bias()
+
+    # Setup the model, data_loader, and HookConfig
+    hooked_mlp = HookedModel(mlp)
+    data = torch.zeros(10, 3)
+    targets = torch.ones(10)
+    zero_data_loader = DataLoader(TensorDataset(data, targets), batch_size=7)
+
+    # Test with no module_names should raise assertion
+    with pytest.raises(AssertionError):
+        collect_dataset_means(hooked_mlp, [], zero_data_loader, device=device, dtype=dtype)
+
+    module_names = ["layers.0", "layers.1"]
+    means, index_pos = collect_dataset_means(
+        hooked_mlp, module_names, zero_data_loader, device=device, dtype=dtype
+    )
+
+    # Ensure hooked_mlp.hooked_data got removed after collect_gram_matrices
+    assert not hooked_mlp.hooked_data
+
+    assert torch.allclose(means["layers.0"], torch.tensor([0.0, 0.0, 0.0, 1.0]))
+    mean_l1 = torch.concatenate([torch.clip(b0, min=0), torch.tensor([1.0])])
+    assert torch.allclose(means["layers.1"], mean_l1)
+    assert index_pos["layers.0"] == torch.tensor([mlp.input_size])
+    assert index_pos["layers.1"] == torch.tensor([mlp.hidden_sizes[0]])

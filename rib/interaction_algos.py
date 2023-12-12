e18ee504c3487d1eeb61c13e45f59b98c7aa2c64
Jake Mendel <jake@apolloresearch.ai>
2023-12-12 20:53:03 +0000

    implemented calculate_interaction_rotations_global in interaction_algos, currently only for the g*f basis (gradient flow). This is NOT tested yet (it has not even been run once)

--- rib/interaction_algos.py
+++ rib/interaction_algos.py
@@ -7,11 +7,15 @@ from typing import Literal, Optional
 import torch
 from jaxtyping import Float, Int
 from torch import Tensor
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 
-from rib.data_accumulator import collect_M_dash_and_Lambda_dash
-from rib.hook_manager import HookedModel
+from rib.data_accumulator import (
+    collect_Lambda_dash,
+    collect_M_dash,
+    collect_M_dash_and_Lambda_dash,
+)
+from rib.hook_manager import Hook, HookedModel
 from rib.linalg import eigendecompose, pinv_diag
 from rib.models.mlp import MLP
 from rib.models.sequential_transformer.transformer import SequentialTransformer
@@ -95,7 +99,7 @@ def build_sorted_lambda_matrices(
     return lambda_matrix, lambda_matrix_pinv
 
 
-def calculate_interaction_rotations(
+def calculate_interaction_rotations_local(
     gram_matrices: dict[str, Float[Tensor, "d_hidden d_hidden"]],
     section_names: list[str],
     node_layers: list[str],
@@ -142,10 +146,11 @@ def calculate_interaction_rotations(
         rotate_final_node_layer: Whether to rotate the final layer to its eigenbasis (which is
             equivalent to its interaction basis). Defaults to True.
         basis_formula: The formula to use for the integrated gradient. Must be one of
-            "(1-alpha)^2" or "(1-0)*alpha". The former is the old (October) version while the
-            latter is a new (November) version that should be used from now on. The latter makes
-            sense especially in light of the new attribution (edge_formula="squared") but is
-            generally good and does not change results much. Defaults to "(1-0)*alpha".
+            "(1-alpha)^2", "(1-0)*alpha", or "g*f". The first is the old (October) version
+            while the second is a new (November) version that should be used from now on. The third
+            is for gradient flow. The second makes sense especially in light of the new attribution
+            (edge_formula="squared") but is generally good and does not change results
+            much. Defaults to "(1-0)*alpha".
         svd_basis: Returns Us as Cs, so basis is just the eigenvectors of the gram matrix.
 
     Returns:
@@ -165,7 +170,8 @@ def calculate_interaction_rotations(
     # We start appending Us and Cs from the output layer and work our way backwards
     Us: list[Eigenvectors] = []
     Cs: list[InteractionRotation] = []
-
+    if basis_formula == "g*f":
+        next_gradients: list = []
     # The C matrix for the final layer is either the eigenvectors U if rotate_final_node_layer is
     # True, and None otherwise
     U_output: Optional[Float[Tensor, "d_hidden d_hidden"]] = (
@@ -302,3 +308,284 @@ def calculate_interaction_rotations(
         )
 
     return Cs[::-1], Us[::-1]
+
+
+def calculate_interaction_rotations_global(
+    gram_matrices: dict[str, Float[Tensor, "d_hidden d_hidden"]],
+    section_names: list[str],
+    node_layers: list[str],
+    hooked_model: HookedModel,
+    data_loader: DataLoader,
+    dtype: torch.dtype,
+    device: str,
+    n_intervals: int,
+    M_dtype: torch.dtype = torch.float64,
+    Lambda_einsum_dtype: torch.dtype = torch.float64,
+    truncation_threshold: float = 1e-5,
+    rotate_final_node_layer: bool = True,
+    basis_formula: Literal["g*f"] = "g*f",
+) -> tuple[list[InteractionRotation], list[Eigenvectors]]:
+    """Calculate the interaction rotation matrices (denoted C) and their psuedo-inverses for a
+    global interaction basis like gradient flow.
+
+    This function implements Algorithm 4(?) of the paper (TODO)
+
+    We collect the interaction rotation matrices from the output layer backwards, as we need the
+    next layer's rotation to compute the current layer's rotation. We reverse the resulting Cs and
+    Us back to the original node order before returning.
+
+    Args:
+        gram_matrices: The gram matrices for each layer, keyed by layer name.
+        section_names: The names of the sections to build the graph from, in order of appearance.
+            Recall that each section is a pytorch module that can be hooked on. For MNIST, or other
+            models without explicit sections, this will simply correspond to layer names.
+        node_layers: Used as a key to store the interaction rotation matrices in the hooked model.
+            May include an optional "output" for the final node layer.
+        hooked_model: The hooked model.
+        data_loader: The data loader.
+        dtype: The data type to use for model computations.
+        device: The device to run the model on.
+        n_intervals: The number of intervals to use for integrated gradients.
+        M_dtype: The data type to use for the M_dash and M matrices, including where the M is
+            collected over the dataset in `M_dash_and_Lambda_dash_pre_forward_hook_fn`. Needs to be
+            float64 for Pythia-14m (empirically). Defaults to float64.
+        Lambda_einsum_dtype: The data type to use for the einsum computing batches for the
+            Lambda_dash matrix. Does not affect the output, only used for the einsum within
+            M_dash_and_Lambda_dash_pre_forward_hook_fn. Needs to be float64 on CPU but float32 was
+            fine on GPU. Defaults to float64.
+        truncation_threshold: Remove eigenvectors with eigenvalues below this threshold.
+        rotate_final_node_layer: Whether to rotate the final layer to its eigenbasis (which is
+            equivalent to its interaction basis). Defaults to True.
+        basis_formula: The formula to use for the integrated gradient. Must be "g*f" for now.
+            This is gradient flow. Defaults to "g*f".
+        svd_basis: Returns Us as Cs, so basis is just the eigenvectors of the gram matrix.
+
+    Returns:
+        - A list of objects containing the interaction rotation matrices and their pseudoinverses,
+        ordered by node layer appearance in model.
+        - A list of objects containing the eigenvectors of each node layer, ordered by node layer
+        appearance in model.
+    """
+    assert hooked_model.model.has_folded_bias, "Biases must be folded in to calculate Cs."
+    assert len(section_names) > 0, "No sections specified."
+
+    non_output_node_layers = [node_layer for node_layer in node_layers if node_layer != "output"]
+    assert len(non_output_node_layers) == len(
+        section_names
+    ), "Must specify a hook name for each section (except the output section)."
+
+    # We start appending Us and Cs from the output layer and work our way backwards
+    Us: list[Eigenvectors] = []
+    Cs: list[InteractionRotation] = []
+    next_gradients: list = []
+
+    # The C matrix for the final layer is either the eigenvectors U if rotate_final_node_layer is
+    # True, and None otherwise
+    U_output: Optional[Float[Tensor, "d_hidden d_hidden"]] = (
+        eigendecompose(gram_matrices[node_layers[-1]])[1].detach().cpu()
+        if rotate_final_node_layer
+        else None
+    )
+    C_output: Optional[Float[Tensor, "d_hidden d_hidden"]] = (
+        U_output.detach().cpu() if U_output is not None else None
+    )
+
+    if node_layers[-1] not in gram_matrices:
+        # Technically we don't actually need the final node layer to be in gram_matrices if we're
+        # not rotating it, but for now, our implementation assumes that it always is unless
+        # final_node_layer is the logits (i.e. ="output").
+        assert (
+            node_layers[-1] == "output"
+        ), f"Final node layer {node_layers[-1]} not in gram matrices."
+
+        inner_model = hooked_model.model
+        if isinstance(inner_model, MLP):
+            final_node_dim = inner_model.output_size
+        else:
+            assert isinstance(inner_model, SequentialTransformer)
+            final_node_dim = inner_model.cfg.d_vocab
+    else:
+        final_node_dim = gram_matrices[node_layers[-1]].shape[0]
+
+    Us.append(
+        Eigenvectors(
+            node_layer_name=node_layers[-1],
+            out_dim=final_node_dim,
+            U=U_output,
+        )
+    )
+    Cs.append(
+        InteractionRotation(
+            node_layer_name=node_layers[-1],
+            out_dim=final_node_dim,
+            C=C_output,
+        )
+    )
+    if U_output is not None:
+        assert C_output is not None
+        assert (
+            C_output.shape[1] == final_node_dim
+        ), f"Expected C_output to have shape (_, {final_node_dim}). Got {C_output.shape}."
+        assert (
+            U_output.shape[1] == final_node_dim
+        ), f"Expected U_output to have shape (_, {final_node_dim}). Got {U_output.shape}."
+
+    # We only need to calculate C for the final section if there is no output node layer
+    section_names_to_calculate = (
+        section_names if node_layers[-1] == "output" else section_names[:-1]
+    )
+
+    assert (
+        len(section_names_to_calculate) == len(node_layers) - 1
+    ), "Must be a section name for all but the final node_layer which was already handled above."
+
+    module_names = section_names_to_calculate
+    hook_names = module_names
+
+    M_matrices = collect_M_dash(
+        hooked_model=hooked_model,
+        section_names=section_names_to_calculate,
+        node_layers=node_layers,
+        n_intervals=n_intervals,
+        data_loader=data_loader,
+        device=device,
+        dtype=dtype,
+        M_dtype=M_dtype,
+        hook_names=hook_names,
+        basis_formula=basis_formula,
+    )
+
+    # Since we've already handled the last node layer, we can ignore it in the loop
+    for node_layer, section_name in tqdm(
+        zip(node_layers[-2::-1], section_names_to_calculate[::-1]),
+        total=len(section_names_to_calculate),
+        desc="Interaction rotations",
+    ):
+        D_dash, U_dash = eigendecompose(gram_matrices[node_layer])
+
+        n_small_eigenvals: int = int(torch.sum(D_dash < truncation_threshold).item())
+        # Truncate the D matrix to remove small eigenvalues
+        D: Float[Tensor, "d_hidden_trunc d_hidden_trunc"] = (
+            torch.diag(D_dash)[:-n_small_eigenvals, :-n_small_eigenvals]
+            if n_small_eigenvals > 0
+            else torch.diag(D_dash)
+        )
+        # Truncate the columns of U to remove small eigenvalues
+        U: Float[Tensor, "d_hidden d_hidden_trunc"] = (
+            U_dash[:, :-n_small_eigenvals] if n_small_eigenvals > 0 else U_dash
+        )
+        Us.append(Eigenvectors(node_layer_name=node_layer, out_dim=U.shape[1], U=U.detach().cpu()))
+        if basis_formula == "svd":
+            # Use U as C and then progress to the next loop
+            Cs.append(
+                InteractionRotation(node_layer_name=node_layer, out_dim=U.shape[1], C=U, C_pinv=U.T)
+            )
+            continue
+
+        U_D_sqrt: Float[Tensor, "d_hidden d_hidden_trunc"] = U @ D.sqrt()
+        M_dash = M_matrices[node_layer]
+        # Converts M to fp64
+        M: Float[Tensor, "d_hidden_trunc d_hidden_trunc"] = (
+            U_D_sqrt.T.to(M_dtype) @ M_dash @ U_D_sqrt.to(M_dtype)
+        )
+        V = eigendecompose(M)[1]  # V has size (d_hidden_trunc, d_hidden_trunc)
+        V = V.to(dtype)
+
+        # Multiply U_D_sqrt with V, corresponding to $U D^{1/2} V$ in the paper.
+        U_D_sqrt_V: Float[Tensor, "d_hidden d_hidden_trunc"] = U_D_sqrt @ V
+        D_sqrt_pinv: Float[Tensor, "d_hidden_trunc d_hidden_trunc"] = pinv_diag(D.sqrt())
+        U_D_sqrt_pinv_V: Float[Tensor, "d_hidden d_hidden_trunc"] = U @ D_sqrt_pinv @ V
+
+        # now calculate Lambda_dash
+        C_out = Cs[-1].C.to(device=device) if Cs[-1].C is not None else None
+        Lambda_dash = collect_Lambda_dash(
+            C_out=C_out,
+            hooked_model=hooked_model,
+            n_intervals=n_intervals,
+            data_loader=data_loader,
+            module_name=section_name,  # TODO: check if this is correct. Should it be node_layer?
+            dtype=dtype,
+            device=device,
+            hook_name=node_layer,
+            Lambda_einsum_dtype=Lambda_einsum_dtype,
+            basis_formula="(1-0)*alpha",
+        )
+
+        Lambda_abs: Float[Tensor, "d_hidden_trunc"] = (
+            (U_D_sqrt_V.T @ Lambda_dash @ U_D_sqrt_pinv_V).diag().abs()
+        )
+
+        Lambda_abs_sqrt_trunc, Lambda_abs_sqrt_trunc_pinv = build_sorted_lambda_matrices(
+            Lambda_abs, truncation_threshold
+        )
+
+        C: Float[Tensor, "d_hidden d_hidden_extra_trunc"] = (
+            (U_D_sqrt_pinv_V @ Lambda_abs_sqrt_trunc).detach().cpu()
+        )
+        C_pinv: Float[Tensor, "d_hidden_extra_trunc d_hidden"] = (
+            (Lambda_abs_sqrt_trunc_pinv @ U_D_sqrt_V.T).detach().cpu()
+        )
+        Cs.append(
+            InteractionRotation(node_layer_name=node_layer, out_dim=C.shape[1], C=C, C_pinv=C_pinv)
+        )
+
+    return Cs[::-1], Us[::-1]
+
+
+LOCAL_BASIS_FORMULAE = ["(1-alpha)^2", "(1-0)*alpha", "svd"]
+GLOBAL_BASIS_FORMULAE = ["g*f"]
+
+
+def calculate_interaction_rotations(
+    gram_matrices: dict[str, Float[Tensor, "d_hidden d_hidden"]],
+    section_names: list[str],
+    node_layers: list[str],
+    hooked_model: HookedModel,
+    data_loader: DataLoader,
+    dtype: torch.dtype,
+    device: str,
+    n_intervals: int,
+    M_dtype: torch.dtype = torch.float64,
+    Lambda_einsum_dtype: torch.dtype = torch.float64,
+    truncation_threshold: float = 1e-5,
+    rotate_final_node_layer: bool = True,
+    basis_formula: Literal["(1-alpha)^2", "(1-0)*alpha", "svd", "g*f"] = "(1-0)*alpha",
+) -> tuple[list[InteractionRotation], list[Eigenvectors]]:
+    """
+    Calls either calculate_interaction_rotations_local or calculate_interaction_rotations_global
+    depending on whether the basis is local or global.
+    """
+    if basis_formula in LOCAL_BASIS_FORMULAE:
+        return calculate_interaction_rotations_local(
+            gram_matrices=gram_matrices,
+            section_names=section_names,
+            node_layers=node_layers,
+            hooked_model=hooked_model,
+            data_loader=data_loader,
+            dtype=dtype,
+            device=device,
+            n_intervals=n_intervals,
+            M_dtype=M_dtype,
+            Lambda_einsum_dtype=Lambda_einsum_dtype,
+            truncation_threshold=truncation_threshold,
+            rotate_final_node_layer=rotate_final_node_layer,
+            basis_formula=basis_formula,
+        )
+    elif basis_formula in GLOBAL_BASIS_FORMULAE:
+        return calculate_interaction_rotations_global(
+            gram_matrices=gram_matrices,
+            section_names=section_names,
+            node_layers=node_layers,
+            hooked_model=hooked_model,
+            data_loader=data_loader,
+            dtype=dtype,
+            device=device,
+            n_intervals=n_intervals,
+            M_dtype=M_dtype,
+            Lambda_einsum_dtype=Lambda_einsum_dtype,
+            truncation_threshold=truncation_threshold,
+            rotate_final_node_layer=rotate_final_node_layer,
+            basis_formula=basis_formula,
+        )
+    else:
+        raise ValueError(f"Unrecognized basis formula {basis_formula}")

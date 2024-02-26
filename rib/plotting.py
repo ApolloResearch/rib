750fdf37e0ce7c79effc7a254722bd90c668758e
Nix Goldowsky-Dill <nix@apolloresearch.ai>
2024-02-26 15:06:10 +0000

    more customizable plotting

--- rib/plotting.py
+++ rib/plotting.py
@@ -68,7 +68,10 @@ def _add_edges_to_graph(
 
 
 def _prepare_edges_for_plotting(
-    raw_edges: list[torch.Tensor], nodes_per_layer: list[int], hide_const_edges: bool = False
+    raw_edges: list[torch.Tensor],
+    nodes_per_layer: list[int],
+    hide_const_edges: bool = False,
+    norm: Optional[float] = None,
 ) -> list[torch.Tensor]:
     """Convert edges to float, normalize, and truncate to desired number of nodes in each layer.
 
@@ -90,7 +93,8 @@ def _prepare_edges_for_plotting(
             # should be zero except for a non-rotated last layer where they are important.
             weight_matrix[:, const_node_index] = 0
         # Normalize the edge weights by the sum of the absolute values of the weights
-        weight_matrix /= torch.sum(torch.abs(weight_matrix))
+        norm = torch.sum(torch.abs(weight_matrix)).item() if norm is None else norm
+        weight_matrix /= norm
         # Only keep the desired number of nodes in each layer
         in_nodes = nodes_per_layer[i]
         out_nodes = nodes_per_layer[i + 1]
@@ -183,6 +187,9 @@ def plot_rib_graph(
     out_file: Optional[Path] = None,
     node_labels: Optional[list[list[str]]] = None,
     hide_const_edges: bool = False,
+    ax=None,
+    norm: Optional[float] = None,
+    colors: Optional[list[str]] = None,
 ) -> None:
     """Plot the RIB graph for the given edges.
 
@@ -203,13 +210,14 @@ def plot_rib_graph(
     max_layer_height = max(nodes_per_layer)
 
     edges = _prepare_edges_for_plotting(
-        raw_edges, nodes_per_layer, hide_const_edges=hide_const_edges
+        raw_edges, nodes_per_layer, hide_const_edges=hide_const_edges, norm=norm
     )
 
     # Create the undirected graph
     graph = nx.Graph()
 
-    fig, ax = plt.subplots(1, 1, figsize=(20, 10))
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=(20, 10))
 
     layers = _create_node_layers(edges)
     # Add nodes to the graph object
@@ -222,19 +230,20 @@ def plot_rib_graph(
     pos: dict[int, tuple[int, Union[int, float]]] = {}
     for i, layer in enumerate(layers):
         # Add extra spacing for nodes that have fewer nodes than the biggest layer
-        spacing = 1 if i == 0 else max_layer_height / len(layer)
+        spacing = max_layer_height / len(layer)
         for j, node in enumerate(layer):
             pos[node] = (i, j * spacing)
 
     # Draw nodes
-    colors = ["black", "green", "orange", "purple"]  # Add more colors if you have more layers
+    if colors is None:
+        colors = ["black", "green", "orange", "purple"]  # Add more colors if you have more layers
     options = {"edgecolors": "tab:gray", "node_size": 100, "alpha": 0.3}
     for i, (layer_name, layer) in enumerate(zip(layer_names, layers)):
         nx.draw_networkx_nodes(
-            graph, pos, nodelist=layer, node_color=colors[i % len(colors)], **options
+            graph, pos, nodelist=layer, node_color=colors[i % len(colors)], ax=ax, **options
         )
         # Add layer label above the nodes
-        plt.text(i, max_layer_height, layer_name, ha="center", va="center", fontsize=12)
+        ax.text(i, max_layer_height, layer_name, ha="center", va="center", fontsize=12)
 
     # Label nodes if node_labels is provided
     if node_labels is not None:
@@ -242,7 +251,7 @@ def plot_rib_graph(
         for i, layer in enumerate(layers):
             for j, node in enumerate(layer):
                 node_label_dict[node] = node_labels[i][j].replace("|", "\n")
-        nx.draw_networkx_labels(graph, pos, node_label_dict, font_size=8)
+        nx.draw_networkx_labels(graph, pos, node_label_dict, font_size=8, ax=ax)
 
     # Draw edges
     width_factor = 15
@@ -253,6 +262,7 @@ def plot_rib_graph(
         width=[width_factor * edge[2]["weight"] for edge in graph.edges(data=True)],
         alpha=1,
         edge_color=[edge[2]["color"] for edge in graph.edges(data=True)],
+        ax=ax,
     )
 
     plt.suptitle(exp_name)

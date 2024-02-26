5d8636acf9e09d56be1971f1f951994f991f7614
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-02-26 14:33:04 +0000

    Variance fix warning

--- rib/models/components.py
+++ rib/models/components.py
@@ -10,6 +10,8 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor, nn
 from torch.nn import functional as F
 
+from rib.log import logger
+
 if TYPE_CHECKING:
     from rib.models import SequentialTransformerConfig
 
@@ -666,7 +668,12 @@ class LayerNormOut(torch.nn.Module):
         # case to return bad loss if we find a negative variance, we think it's easier to just set
         # negative variances to zero -- this should suitably blow up the layer norm scale, and thus
         # produce a bad loss if and only if the layer norm scale was important.
-        var = torch.relu(var)
+        if torch.any(var <= 0):
+            logger.warning(
+                "Negative variance found. This should only occur in edge ablations."
+                "Setting var to ReLU(var)."
+            )
+            var = torch.relu(var)
 
         out = layer_norm(residual_for_norm, var=var + self.cfg.eps)
 
@@ -765,7 +772,12 @@ class DualLayerNormOut(torch.nn.Module):
         # case to return bad loss if we find a negative variance, we think it's easier to just set
         # negative variances to zero -- this should suitably blow up the layer norm scale, and thus
         # produce a bad loss if and only if the layer norm scale was important.
-        var = torch.relu(var)
+        if torch.any(var <= 0):
+            logger.warning(
+                "Negative variance found. This should only occur in edge ablations."
+                "Setting var to ReLU(var)."
+            )
+            var = torch.relu(var)
 
         out = layer_norm(residual_for_norm, var=var + self.cfg.eps)
         if self._exclude_final_dim:

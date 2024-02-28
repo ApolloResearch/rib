ApolloResearch/rib

commit d88adcf84901718e86df2cc8ab3445b7b9be7665

Feature/plotting improvements (#348)
* more customizable plotting

* plot by layer

* Update rib_scripts/rib_build/plot_graph.py

* small fixes

rib/plotting.py
@@ -68,7 +68,10 @@ def _add_edges_to_graph(
 
 
 def _prepare_edges_for_plotting(
-    raw_edges: list[torch.Tensor], nodes_per_layer: list[int], hide_const_edges: bool = False
+    raw_edges: list[torch.Tensor],
+    nodes_per_layer: list[int],
+    hide_const_edges: bool = False,
+    norm: Optional[float] = None,
 ) -> list[torch.Tensor]:
     """Convert edges to float, normalize, and truncate to desired number of nodes in each layer.
 
@@ -76,6 +79,8 @@ def _prepare_edges_for_plotting(
         raw_edges (list[torch.Tensor]): List of edges tensors, each with shape
             (n_nodes_in_l+1, n_nodes_in_l).
         nodes_per_layer (list[int]): The number of nodes in each layer.
+        norm (Optional[float]): The value to normalize the edges by. If None, will normalize each
+            layer by the sum of the absolute values of the edges.
 
     Returns:
         list[torch.Tensor]: A list of edges, each with shape (n_nodes_in_l+1, n_nodes_in_l).
@@ -90,7 +95,8 @@ def _prepare_edges_for_plotting(
             # should be zero except for a non-rotated last layer where they are important.
             weight_matrix[:, const_node_index] = 0
         # Normalize the edge weights by the sum of the absolute values of the weights
-        weight_matrix /= torch.sum(torch.abs(weight_matrix))
+        norm = torch.sum(torch.abs(weight_matrix)).item() if norm is None else norm
+        weight_matrix /= norm
         # Only keep the desired number of nodes in each layer
         in_nodes = nodes_per_layer[i]
         out_nodes = nodes_per_layer[i + 1]
@@ -183,6 +189,9 @@ def plot_rib_graph(
     out_file: Optional[Path] = None,
     node_labels: Optional[list[list[str]]] = None,
     hide_const_edges: bool = False,
+    ax: Optional[plt.Axes] = None,
+    const_edge_norm: Optional[float] = None,
+    colors: Optional[list[str]] = None,
 ) -> None:
     """Plot the RIB graph for the given edges.
 
@@ -194,7 +203,14 @@ def plot_rib_graph(
         nodes_per_layer (Union[int, list[int]]): The number of nodes in each layer. If int, then
             all layers have the same number of nodes. If list, then the number of nodes in each
             layer is given by the list.
-        out_file (Path): The file to save the plot to.
+        out_file (Path): The file to save the plot to. If None, no plot is saved
+        node_labels: The labels for each node in the graph. If None, then no labels are added.
+        hide_const_edges (bool): Whether to hide the outgoing edges from constant nodes.
+        ax: The axis to plot the graph on. If None, then a new figure is created.
+        const_edge_norm (Optional[float]): The value to normalize the edges by. If None, will choose
+            a different value for each layer (the sum of the absolute values of the edges).
+        colors (Optional[list[str]]): The colors to use for the nodes in each layer. If None, then
+            the tab10 colormap is used.
     """
     if isinstance(nodes_per_layer, int):
         # Note that there is one more layer than there edge matrices
@@ -203,13 +219,14 @@ def plot_rib_graph(
     max_layer_height = max(nodes_per_layer)
 
     edges = _prepare_edges_for_plotting(
-        raw_edges, nodes_per_layer, hide_const_edges=hide_const_edges
+        raw_edges, nodes_per_layer, hide_const_edges=hide_const_edges, norm=const_edge_norm
     )
 
     # Create the undirected graph
     graph = nx.Graph()
 
-    fig, ax = plt.subplots(1, 1, figsize=(20, 10))
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=(20, 10))
 
     layers = _create_node_layers(edges)
     # Add nodes to the graph object
@@ -222,19 +239,23 @@ def plot_rib_graph(
     pos: dict[int, tuple[int, Union[int, float]]] = {}
     for i, layer in enumerate(layers):
         # Add extra spacing for nodes that have fewer nodes than the biggest layer
-        spacing = 1 if i == 0 else max_layer_height / len(layer)
+        spacing = max_layer_height / len(layer)
         for j, node in enumerate(layer):
             pos[node] = (i, j * spacing)
 
     # Draw nodes
-    colors = ["black", "green", "orange", "purple"]  # Add more colors if you have more layers
+    if colors is None:
+        # tab10 colormap
+        # convert from rgb to hex to avoid matplotlib warning
+        to_hex = lambda x: f"{int(x * 255):02x}"
+        colors = [f"#{to_hex(r)}{to_hex(g)}{to_hex(b)}" for r, g, b in plt.get_cmap("tab10").colors]  # type: ignore
     options = {"edgecolors": "tab:gray", "node_size": 100, "alpha": 0.3}
     for i, (layer_name, layer) in enumerate(zip(layer_names, layers)):
         nx.draw_networkx_nodes(
-            graph, pos, nodelist=layer, node_color=colors[i % len(colors)], **options
+            graph, pos, nodelist=layer, node_color=colors[i % len(colors)], ax=ax, **options
         )
         # Add layer label above the nodes
-        plt.text(i, max_layer_height, layer_name, ha="center", va="center", fontsize=12)
+        ax.text(i, max_layer_height, layer_name, ha="center", va="center", fontsize=12)
 
     # Label nodes if node_labels is provided
     if node_labels is not None:
@@ -242,7 +263,7 @@ def plot_rib_graph(
         for i, layer in enumerate(layers):
             for j, node in enumerate(layer):
                 node_label_dict[node] = node_labels[i][j].replace("|", "\n")
-        nx.draw_networkx_labels(graph, pos, node_label_dict, font_size=8)
+        nx.draw_networkx_labels(graph, pos, node_label_dict, font_size=8, ax=ax)
 
     # Draw edges
     width_factor = 15
@@ -253,6 +274,7 @@ def plot_rib_graph(
         width=[width_factor * edge[2]["weight"] for edge in graph.edges(data=True)],
         alpha=1,
         edge_color=[edge[2]["color"] for edge in graph.edges(data=True)],
+        ax=ax,
     )
 
     plt.suptitle(exp_name)

rib_scripts/rib_build/plot_graph.py
@@ -3,16 +3,17 @@
 Usage:
     python plot_graph.py <path/to/results.pt> [--nodes_per_layer <int>]
         [--labels_file <path/to/labels.csv>] [--out_file <path/to/out.png>]
-        [--force]
+        [--force] [--by_layer] [--edge_norm_factor <float>]
 
     The results.pt should be the output of the run_rib_build.py script.
 """
 
 import csv
 from pathlib import Path
-from typing import Optional, Union
+from typing import Callable, Optional, Union
 
 import fire
+import matplotlib.pyplot as plt
 import torch
 
 from rib.log import logger
@@ -20,19 +21,114 @@ from rib.plotting import plot_rib_graph
 from rib.rib_builder import RibBuildResults
 from rib.utils import check_out_file_overwrite, handle_overwrite_fail
 
+ResultsLike = Union[RibBuildResults, Path, str]
+
+
+def _to_results(results: ResultsLike) -> RibBuildResults:
+    if isinstance(results, RibBuildResults):
+        return results
+    elif isinstance(results, (str, Path)):
+        return RibBuildResults(**torch.load(results))
+    else:
+        raise ValueError(f"Invalid results type: {type(results)}")
+
+
+def plot_by_layer(
+    results: ResultsLike,
+    nodes_per_layer=100,
+    out_file=None,
+    edge_norm: Optional[Callable[[torch.Tensor, str], torch.Tensor]] = None,
+    hide_const_edges: bool = True,
+    const_edge_norm: Optional[float] = None,
+):
+    """
+    Plots a RIB graph with every transformer block on it's own row.
+
+    Can be called from the command line interface with `--by_layer` flag.
+
+    You'll need to call the function from python if you want to use edge_norm.
+
+    Args:
+        results: The results file containing the graph edges.
+        nodes_per_layer: The number of nodes per layer to plot.
+        out_file: The path to save the plot.
+        edge_norm: A function to normalize the edge weights pre-plotting.
+        hide_const_edges: Whether to hide the nodes corresponding to constant RIB dirs. This is
+            ignored if the RIB build is non-centered
+        const_edge_norm: If non-none, will use a fixed normalization value for all layers instead
+            of normalizing edges layer by layer.
+    """
+    results = _to_results(results)
+
+    def get_block(name: str) -> Optional[int]:
+        split = name.split(".")
+        if len(split) == 2:
+            return int(split[1])
+        else:
+            return None
+
+    blocks_in_results = [
+        get_block(nl) for nl in results.config.node_layers if get_block(nl) is not None
+    ]
+    assert blocks_in_results
+    blocks = range(min(blocks_in_results), max(blocks_in_results) + 1)  # type: ignore
+
+    fig, axs = plt.subplots(len(blocks), 1, figsize=(8, len(blocks) * 6))
+    axs = axs if len(blocks) > 1 else [axs]
+
+    for ax, block in zip(axs, blocks, strict=True):
+        edges = [edge for edge in results.edges if get_block(edge.in_node_layer) == block]
+        layers = [edge.in_node_layer for edge in edges] + [edges[-1].out_node_layer]
+        if edge_norm is not None:
+            raw_edges = [edge_norm(edge.E_hat, edge.in_node_layer) for edge in edges]
+        else:
+            raw_edges = [edge.E_hat for edge in edges]
+
+        plot_rib_graph(
+            raw_edges=raw_edges,
+            layer_names=layers,
+            exp_name=results.exp_name,
+            nodes_per_layer=nodes_per_layer,
+            out_file=None,
+            node_labels=None,
+            hide_const_edges=results.config.center and hide_const_edges,
+            ax=ax,
+            const_edge_norm=const_edge_norm,
+        )
+
+    if out_file is not None:
+        plt.savefig(out_file, dpi=300)
+
+        logger.info(f"Saved plot to {Path(out_file).absolute()}")
+
 
 def main(
-    results_file: str,
-    nodes_per_layer: Union[int, list[int]] = 40,
+    results: ResultsLike,
+    nodes_per_layer: Union[int, list[int]] = 64,
     labels_file: Optional[str] = None,
     out_file: Optional[Union[str, Path]] = None,
     force: bool = False,
-    hide_const_edges: Optional[bool] = None,
+    hide_const_edges: bool = True,
+    by_layer: Optional[bool] = False,
+    const_edge_norm: Optional[float] = None,
 ) -> None:
-    """Plot an RIB graph given a results file contain the graph edges."""
-    results = RibBuildResults(**torch.load(results_file))
-    out_dir = Path(__file__).parent / "out"
+    """Plot an RIB graph given a results file contain the graph edges.
+
+    Args:
+        results: The results file containing the graph edges.
+        nodes_per_layer: The number of nodes per layer to plot.
+        labels_file: A csv file containing the labels for the nodes.
+        out_file: The path to save the plot.
+        force: Whether to overwrite the out_file if it already exists.
+        hide_const_edges: Whether to hide the nodes corresponding to constant RIB dirs. This is
+            ignored if the RIB build is non-centered
+        by_layer: Whether to plot the graph by layer.
+        const_edge_norm: If non-none, will use a fixed normalization value for all layers instead
+            of normalizing edges layer by layer.
+    """
+    results = _to_results(results)
     if out_file is None:
+        out_dir = Path(__file__).parent / "out"
         out_file = out_dir / f"{results.exp_name}_rib_graph.png"
     else:
         out_file = Path(out_file)
@@ -41,6 +137,8 @@ def main(
         handle_overwrite_fail()
 
     assert results.edges, "The results file does not contain any edges."
+    hide_const_edges = hide_const_edges or results.config.center
+
     # Add labels if provided
     if labels_file is not None:
         with open(labels_file, "r", newline="") as file:
@@ -48,6 +146,18 @@ def main(
             node_labels = list(reader)
     else:
         node_labels = None
+
+    if by_layer:
+        if node_labels is not None:
+            raise NotImplementedError("Would just need to find the right subset of labels")
+        plot_by_layer(
+            results,
+            nodes_per_layer=nodes_per_layer,
+            out_file=out_file,
+            hide_const_edges=results.config.center and hide_const_edges,
+            const_edge_norm=const_edge_norm,
+        )
+        return None
 
     edge_layers = [edges.in_node_layer for edges in results.edges] + [
         results.edges[-1].out_node_layer
@@ -64,7 +174,8 @@ def main(
         nodes_per_layer=nodes_per_layer,
         out_file=out_file,
         node_labels=node_labels,
-        hide_const_edges=hide_const_edges or results.config.center,
+        hide_const_edges=results.config.center and hide_const_edges,
+        const_edge_norm=const_edge_norm,
     )
 
     logger.info(f"Saved plot to {out_file}")

commit 39da02397d0253cf982bea054c4da57dadb43f1c

Fix for bug introduced in #348

rib/plotting.py
@@ -71,7 +71,7 @@ def _prepare_edges_for_plotting(
     raw_edges: list[torch.Tensor],
     nodes_per_layer: list[int],
     hide_const_edges: bool = False,
-    norm: Optional[float] = None,
+    const_edge_norm: Optional[float] = None,
 ) -> list[torch.Tensor]:
     """Convert edges to float, normalize, and truncate to desired number of nodes in each layer.
 
@@ -95,7 +95,7 @@ def _prepare_edges_for_plotting(
             # should be zero except for a non-rotated last layer where they are important.
             weight_matrix[:, const_node_index] = 0
         # Normalize the edge weights by the sum of the absolute values of the weights
-        norm = torch.sum(torch.abs(weight_matrix)).item() if norm is None else norm
+        norm = const_edge_norm or torch.sum(torch.abs(weight_matrix)).item()
         weight_matrix /= norm
         # Only keep the desired number of nodes in each layer
         in_nodes = nodes_per_layer[i]
@@ -219,7 +219,10 @@ def plot_rib_graph(
     max_layer_height = max(nodes_per_layer)
 
     edges = _prepare_edges_for_plotting(
-        raw_edges, nodes_per_layer, hide_const_edges=hide_const_edges, norm=const_edge_norm
+        raw_edges,
+        nodes_per_layer,
+        hide_const_edges=hide_const_edges,
+        const_edge_norm=const_edge_norm,
     )
 
     # Create the undirected graph
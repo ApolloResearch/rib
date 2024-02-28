811cb89f798758ea1c8d3e21e517f45f36c27c0b
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-02-28 11:44:48 +0000

    Huggingface cache

--- rib/settings.py
+++ rib/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 
+# Huggingface cache
+os.environ["HF_HOME"] = "/mnt/ssd-interp/huggingface_cache/"
+
 REPO_ROOT = (
     Path(os.environ["GITHUB_WORKSPACE"]) if os.environ.get("CI") else Path(__file__).parent.parent
 )

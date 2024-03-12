9b3bfb239036b3d36d78078eb9f7d4a42b7ed96c
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-03-12 11:00:25 +0000

    Undo changes

--- rib_scripts/modularity/run_llm_analysis.py
+++ rib_scripts/modularity/run_llm_analysis.py
@@ -54,15 +54,10 @@ def edge_distribution(
         vlines: A dictionary of node_layer -> float, drawn as vertical lines on the subplots.
     """
     n_layers = _num_layers(results)
-    if n_layers < 2:
-        fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-        axs = np.array([[ax]])
-    else:
-        layout = layout or (n_layers // 4, 4)
-        figsize = (layout[1] * 3, layout[0] * 3)
-        fig, axs = plt.subplots(*layout, figsize=figsize, sharex=True, sharey=True)
-
+    layout = layout or (n_layers // 4, 4)
     qs = torch.cat([torch.linspace(0.01, 0.9, 100), torch.linspace(0.9, 1, 100)])
+    figsize = (layout[1] * 3, layout[0] * 3)
+    fig, axs = plt.subplots(*layout, figsize=figsize, sharex=True, sharey=True)
 
     def get_prefix_colors():
         cmap = plt.get_cmap("Paired")
@@ -73,9 +68,6 @@ def edge_distribution(
             "ln2": cmap.colors[9],
             "ln2_out": cmap.colors[8],
             "mlp_in": cmap.colors[5],
-            "mlp_out": cmap.colors[6],
-            "unembed": cmap.colors[7],
-            "output": cmap.colors[2],
         }
 
     plt.xscale("log")
@@ -83,7 +75,7 @@ def edge_distribution(
         in_nl = edge.in_node_layer
         if in_nl in ["ln_final", "ln_final_out"]:
             continue
-        prefix, layer = in_nl.split(".") if "." in in_nl else (in_nl, 0)
+        prefix, layer = in_nl.split(".")
         E = edge.E_hat.to(torch.float32).cpu().numpy()
         xs = np.quantile(E, qs)  # using np instead of torch as it's much much faster
         ax: plt.Axes = axs.flat[int(layer)]
@@ -205,9 +197,7 @@ def run_modularity(
             plt.clf()
 
     logger.info(f"Making RIB graph in networkit & running clustering...")
-    graph = GraphClustering(
-        results, edge_norm, gamma=gamma, node_layers=["ln1.0", "ln2.0", "unembed"]
-    )
+    graph = GraphClustering(results, edge_norm, gamma=gamma)
     logger.info(f"Finished clustering.")
 
     if plot_piano:

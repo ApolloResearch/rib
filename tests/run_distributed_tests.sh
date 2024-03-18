a71d8a22c8cab8ec268de418abecc2e43e4db6b4
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-03-18 11:48:14 +0000

    Fix distributed CI

--- tests/run_distributed_tests.sh
+++ tests/run_distributed_tests.sh
@@ -1,4 +1,14 @@
-pytest --runmpi tests/test_distributed.py::test_squared_edges_are_same_dist_split_over_dataset
-pytest --runmpi tests/test_distributed.py::test_squared_edges_are_same_dist_split_over_out_dim
-pytest --runmpi tests/test_distributed.py::test_stochastic_edges_are_same_dist_split_over_out_dim
-pytest --runmpi tests/test_distributed.py::test_distributed_basis
+# Run the tests and return exit code 0 if all tests pass, 1 if any fail
+for test in \
+    "pytest --runmpi tests/test_distributed.py::test_squared_edges_are_same_dist_split_over_dataset" \
+    "pytest --runmpi tests/test_distributed.py::test_squared_edges_are_same_dist_split_over_out_dim" \
+    "pytest --runmpi tests/test_distributed.py::test_stochastic_edges_are_same_dist_split_over_out_dim" \
+    "pytest --runmpi tests/test_distributed.py::test_distributed_basis"
+do
+    echo "Running $test"
+    $test
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+done
+exit 0

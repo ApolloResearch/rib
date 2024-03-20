3b4692bab36ebda1f423e487551ce5953d35b6ea
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-03-20 21:41:05 +0000

    Add seed sample configs

--- rib_scripts/rib_build/sample_configs_modadd/run_all.sh
+++ rib_scripts/rib_build/sample_configs_modadd/run_all.sh
@@ -9,20 +9,24 @@ function run_modularity_pipeline {
     ablation_dir="${base_dir}/rib/rib_scripts/ablations"
     interp_dir="${base_dir}/interp/interp/mod_add_interp"
     prefix="modular_arithmetic_"
+    suffix="_seed$2"
 
     cd "${build_dir}"
-    python run_rib_build.py "${config_dir}/${prefix}${type}.yaml"
+    python run_rib_build.py "${config_dir}/${prefix}${type}${suffix}.yaml" -f
 
     cd "${interp_dir}"
-    python generate_labels_mod_add.py "${build_dir}/out/${prefix}${type}_rib_graph.pt" --out_file="${prefix}${type}.csv" --max_dim 140 -f
+    python generate_labels_mod_add.py "${build_dir}/out/${prefix}${type}${suffix}_rib_graph.pt" --out_file="${prefix}${type}${suffix}.csv" --max_dim 50 -f
 
     cd "${ablation_dir}"
-    python run_ablations.py "${config_dir}/ablations_edge_bisect_${prefix}${type}.yaml"
+    python run_ablations.py "${config_dir}/ablations_edge_bisect_${prefix}${type}${suffix}.yaml" -f
 
     cd "${build_dir}"
-    OMP_NUM_THREADS=1 python run_modularity.py "${build_dir}/out/${prefix}${type}_rib_graph.pt" --ablation_path "${ablation_dir}/out/${prefix}${type}_edge_ablation_results.json" --labels_file "${interp_dir}/${prefix}${type}.csv" --gamma 1 --nodes_per_layer 30 --seed 0 --hide_const_edges True --plot_norm abs --figsize 10,14 --sorting cluster
+    OMP_NUM_THREADS=1 python run_modularity.py "${build_dir}/out/${prefix}${type}${suffix}_rib_graph.pt" --ablation_path "${ablation_dir}/out/${prefix}${type}${suffix}_edge_ablation_results.json" --labels_file "${interp_dir}/${prefix}${type}${suffix}.csv" --gamma 1 --nodes_per_layer 30 --seed 0 --hide_const_edges True --plot_norm abs --figsize 10,14 --sorting cluster --line_width_factor 20
 }
 
-run_modularity_pipeline rib
-run_modularity_pipeline pca
-run_modularity_pipeline neuron
+for i in {0..4}
+do
+    run_modularity_pipeline rib $i
+    run_modularity_pipeline pca $i
+done
+# run_modularity_pipeline neuron

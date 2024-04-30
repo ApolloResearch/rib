0947c9091acf866db245eeaf9beca392ab6c2bd0
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-04-30 15:06:36 +0000

    Copy label code from interp; remove hf_cache

--- rib_scripts/ablations/run_ablations.py
+++ rib_scripts/ablations/run_ablations.py
@@ -3,10 +3,6 @@
 Usage:
     python run_ablations.py <path/to/yaml_config_file> [--force]
 """
-# Huggingface cache, needs to be set before importing huggingface things
-from os import environ
-
-environ["HF_HOME"] = "/mnt/ssd-interp/huggingface_cache/"
 
 import builtins
 from functools import wraps
@@ -14,18 +10,21 @@ from pathlib import Path
 
 original_open = builtins.open  # Save the original open function
 
+
 def patch_path(path):
     """Function to replace '/mnt' with '/homt/mnt' in the file path."""
     if isinstance(path, Path):
         path = str(path)
-    path = path.replace('/mnt/ssd-interp/checkpoints/', '/data/stefan_heimersheim/s3/checkpoints/')
+    path = path.replace("/mnt/ssd-interp/checkpoints/", "/data/stefan_heimersheim/s3/checkpoints/")
     return path
+
 
 @wraps(original_open)
 def open_patched(file, *args, **kwargs):
     """Patched open function that modifies the file path."""
     new_file = patch_path(file)
     return original_open(new_file, *args, **kwargs)
+
 
 # Patch the built-in open with our custom function
 builtins.open = open_patched

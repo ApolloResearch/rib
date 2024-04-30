57ce13994ce9d8c1ff85654d159fb7c1046e543f
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-04-30 15:06:36 +0000

    Copy label code from interp; remove hf_cache

--- rib_scripts/ablations/run_ablations.py
+++ rib_scripts/ablations/run_ablations.py
@@ -3,10 +3,6 @@
 Usage:
     python run_ablations.py <path/to/yaml_config_file> [--force]
 """
-# Huggingface cache, needs to be set before importing huggingface things
-from os import environ
-
-environ["HF_HOME"] = "/mnt/ssd-interp/huggingface_cache/"
 
 import fire
 

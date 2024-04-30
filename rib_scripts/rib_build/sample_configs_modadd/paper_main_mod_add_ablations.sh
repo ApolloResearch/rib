b3ebcc2703d50137238157393ae973df378dc5bb
Stefan Heimersheim <stefan@apolloresearch.ai>
2024-04-30 16:31:15 +0000

    Update all configs to new cluster environment

--- rib_scripts/rib_build/sample_configs_modadd/paper_main_mod_add_ablations.sh
+++ rib_scripts/rib_build/sample_configs_modadd/paper_main_mod_add_ablations.sh
@@ -3,22 +3,19 @@ set -e
 set -x
 function run_modularity_pipeline {
     type="$1"
-    config_dir="/data/stefan_heimersheim/s3/rib/rib_scripts/rib_build/sample_configs_modadd"
-    base_dir="/data/stefan_heimersheim/s3/"
+    base_dir="/data/stefan_heimersheim/projects/RIB"
+    config_dir="${base_dir}/rib/rib_scripts/rib_build/sample_configs_modadd"
     build_dir="${base_dir}/rib/rib_scripts/rib_build"
     ablation_dir="${base_dir}/rib/rib_scripts/ablations"
-    interp_dir="${base_dir}/interp/interp/mod_add_interp"
+    out_dir="${config_dir}/out"
     prefix="modular_arithmetic_"
     suffix="_seed$2"
+    run_name="${prefix}${type}${suffix}"
 
-    # cd "${build_dir}"
-    # python run_rib_build.py "${config_dir}/${prefix}${type}${suffix}.yaml" -f
-
-    cd "${ablation_dir}"
-    python run_ablations.py "${config_dir}/ablations_node_exp_${prefix}${type}${suffix}.yaml" -f
-
-    # On branch paper/mod_add_custom_ablation_plots
-    python ${ablation_dir}/plot_ablations.py ${ablation_dir}/out/modular_arithmetic_node_*_rib_ablation_results.json -f
+    python ${build_dir}/run_rib_build.py "${config_dir}/${run_name}.yaml" -f
+    python ${ablation_dir}/run_ablations.py "${config_dir}/ablations_node_exp_${run_name}.yaml" -f
+    # Make sure to run at least this command on branch paper/mod_add_custom_ablation_plots
+    python ${ablation_dir}/plot_ablations.py ${out_dir}/modular_arithmetic_node_*_rib_ablation_results.json -f
 }
 
 for i in {0..4}
